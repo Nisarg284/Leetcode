2e754cc81f9bb947c08b550a96365bac25c36f7e
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-01-19 11:58:26 +0530

    Time: 3 ms (100.00%) | Memory: 79.3 MB (22.58%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,43 +1,63 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
 
-        int n= nums.length;
-        int[] positive = new int[n/2];
-        int[] negative = new int[n/2];
+        // int n = nums.length;
+        int [] ans = new int[nums.length];
 
-        int j=0;
-        int k=0;
-
+        int p=0;
+        int n=1;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i] >  0)
+            if(nums[i] > 0)
             {
-                positive[j] = nums[i];
-                j++;
+                ans[p] = nums[i];
+                p+=2;
             }else{
-                negative[k] = nums[i];
-                k++;
+                ans[n] = nums[i];
+                n+=2;
             }
         }
+        return ans;
 
-        k=0;
-        int i=0;
-        j=0;
-        while(k < n)
-        {
-            if(k%2==0)
-            {
-                nums[k] = positive[i];
-                i++;
-                k++;
-            }else{
-                nums[k] = negative[j];
-                j++;
-                k++;
-            }
-        }
 
-        return nums;
+
+        // int n= nums.length;
+        // int[] positive = new int[n/2];
+        // int[] negative = new int[n/2];
+
+        // int j=0;
+        // int k=0;
+
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i] >  0)
+        //     {
+        //         positive[j] = nums[i];
+        //         j++;
+        //     }else{
+        //         negative[k] = nums[i];
+        //         k++;
+        //     }
+        // }
+
+        // k=0;
+        // int i=0;
+        // j=0;
+        // while(k < n)
+        // {
+        //     if(k%2==0)
+        //     {
+        //         nums[k] = positive[i];
+        //         i++;
+        //         k++;
+        //     }else{
+        //         nums[k] = negative[j];
+        //         j++;
+        //         k++;
+        //     }
+        // }
+
+        // return nums;
         
     }
 }
