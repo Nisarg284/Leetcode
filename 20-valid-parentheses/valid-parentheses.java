013a82106d28aedfae77b2c3628c3155455ac6da
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-01-29 00:24:29 +0530

    Time: 2 ms (97.65%) | Memory: 41.8 MB (66.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char top = st.peek();
+
+                if ((top == '(' && ch == ')') || (top == '{' && ch == '}') || (top == '[' && ch == ']')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (!st.isEmpty()) {
+            return false;
+        }
+
+        return true;
+
+    }
+}
