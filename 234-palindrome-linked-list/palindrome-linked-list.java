a6d3fab4a3c04bd0203d926711b050cfa3f972ef
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-01-30 01:12:57 +0530

    Time: 3 ms (99.76%) | Memory: 68.9 MB (21.94%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,97 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    
+    public boolean isPalindrome(ListNode head) {
+
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null)
+        {
+            slow =slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newLL = slow;
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        ListNode next;
+
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        slow.next = prev;
+
+        ListNode h1 = head;
+        ListNode h2 = prev;
+
+        while(h1!=slow)
+        {
+            if(h1.val!=h2.val)
+            {
+                return false;
+            }
+            h1=h1.next;
+            h2 = h2.next;
+        }
+
+        return true;
+
+        // ListNode slow=head;
+        // ListNode fast=head;
+        
+        // while(fast!=null && fast.next!=null)
+        // {
+        //     slow=slow.next;
+        //     fast=fast.next.next;
+        // }
+        
+        // ListNode mid=slow;
+    
+        
+        // ListNode prev=null;
+        // ListNode curr=mid;
+        // ListNode next;
+        
+        // while(curr!=null)
+        // {
+        //     next=curr.next;
+        //     curr.next=prev;
+        //     prev=curr;
+        //     curr=next;
+        // }
+        
+        // ListNode rightHead=prev;
+        // ListNode leftHead=head;
+        
+        // while(rightHead!=null)
+        // {
+        //     if(leftHead.val!=rightHead.val)
+        //     {
+        //         return false;
+        //     }
+        //     leftHead=leftHead.next;
+        //     rightHead=rightHead.next;
+        // }
+        
+        // return true;
+        
+    }
+}
