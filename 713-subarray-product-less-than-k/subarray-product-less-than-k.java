2d0357571684d603add6c3ba3813e08b1969f245
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-02-17 01:13:47 +0530

    Time: 4 ms (99.95%) | Memory: 47.9 MB (35.36%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -1,6 +1,11 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
 
+        if( k <= 1)
+        {
+            return 0;
+        }
+
         int i=0;
         int j=0;
 
