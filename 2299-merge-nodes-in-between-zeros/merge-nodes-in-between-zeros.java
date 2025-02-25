741fc634c72d268a90e3cd2e71525ce93cbf29c2
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-02-25 16:14:02 +0530

    Time: 5 ms (79.86%) | Memory: 79.1 MB (97.89%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -8,29 +8,56 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeNodes(ListNode head) {
+
+//         ListNode i = head.next;
+
+//         while(i!=null)
+//         {
+//             ListNode j = i;
+
+//             int sum = 0;
+//             while(j!=null && j.val != 0)
+//             {
+//                 sum += j.val;
+//                 j = j.next;
+//             }
+
+//             i.val = sum;
+//             i.next = j.next;
+//             i = j.next;
+           
+//         }
+
+//         return head.next;
+        
+//     }
+// }
+
+
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-
-        ListNode i = head.next;
-
-        while(i!=null)
-        {
-            ListNode j = i;
-
-            int sum = 0;
-            while(j!=null && j.val != 0)
-            {
-                sum += j.val;
-                j = j.next;
-            }
-
-            i.val = sum;
-            i.next = j.next;
-            i = j.next;
-           
-        }
-
-        return head.next;
+        ListNode curr = head.next;  // Skip initial 0
+        ListNode sumNode = curr;    // Node to hold sum
         
+        while (curr != null) {
+            if (curr.val == 0 && sumNode != curr) {  // End of segment
+                sumNode.next = curr.next;            // Skip to next segment
+                sumNode = curr.next;                 // Move to next sum node
+                curr = curr.next;
+            } else {                                // Add to sum
+                if(sumNode == curr)
+                {
+                    curr = curr.next;
+                }else{
+                    sumNode.val += curr.val;
+                    curr = curr.next;
+                }
+                
+            }
+        }
+        
+        return head.next;
     }
 }
