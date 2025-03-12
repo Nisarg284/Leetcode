7dcb0b120861cfefc6f1ae7dca3551306ba6fb70
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-03-12 17:41:31 +0530

    Time: 458 ms (5.03%) | Memory: 45.6 MB (29.27%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,74 +1,10 @@
-// class Solution {
-// public String minWindow(String s, String t) {
-
-//     if(t.length() > s.length())
-//     {
-//         return "";
-//     }
-//         HashMap<Character, Integer> idealMap = new HashMap<>();
-//         HashMap<Character, Integer> actualMap = new HashMap<>();
-
-//         int m = t.length();
-//         int n = s.length();
-
-//         for (int i = 0; i < m; i++) {
-//             char ch = t.charAt(i);
-//             idealMap.put(ch, idealMap.getOrDefault(ch, 0) + 1);
-//         }
-
-//         int len = Integer.MAX_VALUE;
-//         int i = 0;
-//         int j = 0;
-//         int counter = 0;
-//         String ans = "";
-
-//         while (j < n) {
-//             char right = s.charAt(j);
-//             if (idealMap.containsKey(right)) {
-//                 actualMap.put(right, actualMap.getOrDefault(right, 0) + 1);
-
-//                 if (actualMap.get(right) == idealMap.get(right)) {
-//                     counter++;
-//                 }
-//             }
-
-//             while (counter == idealMap.size()) {
-//                 System.out.println("run inner");
-//                 char left = s.charAt(i);
-//                 int currLen = j - i + 1;
-//                 if (currLen < len) {
-//                     len = currLen;
-//                     ans = s.substring(i, j + 1);
-//                 }
-
-//                 if (idealMap.containsKey(left)) {
-
-//                     if (actualMap.get(left) == 1) {
-//                         actualMap.remove(left);
-//                         counter--;
-//                     } else {
-//                         actualMap.put(left, actualMap.get(left) - 1);
-//                         if (actualMap.get(left) < idealMap.get(left)) {
-//                             counter--;
-//                         }
-//                     }
-//                 }
-//                 i++;
-//             }
-//             j++;
-
-//         }
-//         return ans;
-//     }
-
-// }
-
-
 class Solution {
-    public String minWindow(String s, String t) {
-        if (t.length() > s.length()) {
-            return "";
-        }
+public String minWindow(String s, String t) {
+
+    if(t.length() > s.length())
+    {
+        return "";
+    }
         HashMap<Character, Integer> idealMap = new HashMap<>();
         HashMap<Character, Integer> actualMap = new HashMap<>();
 
@@ -90,12 +26,14 @@ class Solution {
             char right = s.charAt(j);
             if (idealMap.containsKey(right)) {
                 actualMap.put(right, actualMap.getOrDefault(right, 0) + 1);
-                if (actualMap.get(right).equals(idealMap.get(right))) {
+
+                if (actualMap.get(right).equals( idealMap.get(right))) {
                     counter++;
                 }
             }
 
             while (counter == idealMap.size()) {
+                System.out.println("run inner");
                 char left = s.charAt(i);
                 int currLen = j - i + 1;
                 if (currLen < len) {
@@ -104,6 +42,7 @@ class Solution {
                 }
 
                 if (idealMap.containsKey(left)) {
+
                     if (actualMap.get(left) == 1) {
                         actualMap.remove(left);
                         counter--;
@@ -117,7 +56,11 @@ class Solution {
                 i++;
             }
             j++;
+
         }
         return ans;
     }
+
 }
+
+
