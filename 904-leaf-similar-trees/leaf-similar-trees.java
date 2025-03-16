8bc257a5f556e1fdfe90de4ba717ec0a91ee3ca9
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-03-17 00:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (41.21%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public static void filling(TreeNode root,ArrayList<Integer> li)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        // leaf node condition
+        if(root.left == null && root.right == null)
+        {
+            li.add(root.val);
+        }
+
+        filling(root.left,li);
+        filling(root.right,li);
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+        if(root1 == null || root2 == null)
+        {
+            return false;
+        }
+
+        ArrayList<Integer> li1 = new ArrayList<>();
+        ArrayList<Integer> li2 = new ArrayList<>();
+        filling(root1,li1);
+        filling(root2,li2);
+
+        return li1.equals(li2);
+
+        
+    }
+}
