9b47113fe4601b897c6ae775ae226e88754517be
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-03-25 00:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (22.81%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,33 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
 
-    public static TreeNode  IOP(TreeNode root)
+    public static TreeNode findPred(TreeNode root)
     {
-        // once go left
-        TreeNode pred = root.left;
-
-        // go right until pred.right != null
-        while(pred.right != null)
+        while(root.right!=null)
         {
-            pred = pred.right; 
+            root=root.right;
         }
-        return null;
 
-    }
-
-    public static TreeNode  IOS(TreeNode root)
-    {
-        // once go right
-        TreeNode succ = root.right;
-
-        // go left until pred.left != null
-        while(succ.left != null)
-        {
-            succ = succ.left; 
-        }
-        return succ;
-
+        return root;
     }
     public TreeNode deleteNode(TreeNode root, int key) {
+
         if(root == null)
         {
             return null;
@@ -35,131 +33,45 @@ class Solution {
 
         if(root.val == key)
         {
-            // Case 1: delete Leaf node
             if(root.left == null && root.right == null)
             {
-               // root = null;
                 return null;
             }
 
-            // case 2: root has only 1 child node
             if(root.left == null || root.right == null)
             {
-                // find not null node
-                if(root.left != null)
-                {
-                    return root.left;
-                }
-
-                if(root.right != null)
+                if(root.left == null)
                 {
                     return root.right;
                 }
+
+                if(root.right == null)
+                {
+                    return root.left;
+                }
             }
 
-            // Case 3: root has 2 child nodes
             if(root.left != null && root.right != null)
             {
-                // find predecessor  / successor  
-                TreeNode succ = IOS(root);
+                TreeNode pred = findPred(root.left);
 
-                // Replace root value with pred value
-                root.val = succ.val;
-
-                // delete pred node from left side of tree
-                root.right = deleteNode(root.right,succ.val);
-
-                //return root; 
+                int rootVal = pred.val;
+                pred.val = root.val;
+                root.val = rootVal;
+                 root.left = deleteNode(root.left,pred.val);
+                 return root;
             }
-
         }
-      
-        // if root.val > key so go left
-        else if(root.val > key)
+
+        if(root.val < key)
         {
-            // attached & call left subtree
-           root.left = deleteNode(root.left,key);
-
+           root.right =  deleteNode(root.right,key);
+        }else{
+            root.left = deleteNode(root.left,key);
         }
-        else
-        { //  root.val < key,  so go right
 
-            // attached & call right subtree
-            root.right = deleteNode(root.right,key);
-
-        }
         return root;
 
+        
     }
 }
-
-
-
-
-// class Solution {
-
-//     public static TreeNode IOP(TreeNode root) {
-//         // once go left
-//         TreeNode pred = root.left;
-
-//         // go right until pred.right == null
-//         while (pred.right != null) {
-//             pred = pred.right;
-//         }
-//         return pred;
-//     }
-
-//     public static TreeNode IOS(TreeNode root) {
-//         // once go right
-//         TreeNode succ = root.right;
-
-//         // go left until succ.left == null
-//         while (succ.left != null) {
-//             succ = succ.left;
-//         }
-//         return succ;
-//     }
-
-//     public TreeNode deleteNode(TreeNode root, int key) {
-//         if (root == null) {
-//             return null;
-//         }
-
-//         if (root.val == key) {
-//             // Case 1: delete Leaf node
-//             if (root.left == null && root.right == null) {
-//                 return null;
-//             }
-
-//             // Case 2: root has only 1 child node
-//             if (root.left == null) {
-//                 return root.right;
-//             }
-
-//             if (root.right == null) {
-//                 return root.left;
-//             }
-
-//             // Case 3: root has 2 child nodes
-//             // Find successor
-//             TreeNode succ = IOS(root);
-//             // Replace root value with succ value
-//             root.val = succ.val;
-//             // Delete succ node from right side of tree
-//             root.right = deleteNode(root.right, succ.val);
-//         } else if (root.val > key) {
-//             // if root.val > key go left
-//             root.left = deleteNode(root.left, key);
-//         } else {
-//             // root.val < key, go right
-//             root.right = deleteNode(root.right, key);
-//         }
-//         return root;
-//     }
-// }
-
-
-
-
-
-
