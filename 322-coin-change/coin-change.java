d97f34ba0e86dfdbdf183db3fa4e85e57d61acab
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-05-09 00:57:01 +0530

    Time: 25 ms (33.73%) | Memory: 45.5 MB (15.55%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,34 +1,37 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
 
-        int [] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
+        int n = coins.length;
 
-        for(int i= 1;i<=amount;i++)
-        {
-            for(int j=0;j<coins.length;j++)
-            {
-                if(i - coins[j] >= 0)
-                {
-                    // while(amount >= coins[i-1])
-                    // {
-                    //     dp[i][j] = 1 + dp[i-1][j-1];
-                    // amount -= coins[i-1];
-                    // }
-
-                    dp[i] = Math.min(dp[i],1+dp[i-coins[j]]);
-                    
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) {
+                    dp[i][j] = 0;
+                } else if (i == 0) {
+                    dp[i][j] = Integer.MAX_VALUE - 1;
                 }
             }
         }
 
-        if(dp[amount] != amount +1)
-        {
-            return dp[amount];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    int accept = dp[i][j - coins[i - 1]];
+                    int reject = dp[i - 1][j];
+
+                    if (dp[i][j] != Integer.MAX_VALUE - 1) {
+                        dp[i][j] = Math.min(1 + accept, reject);
+                    } else {
+                        dp[i][j] = reject;
+                    }
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
 
-        return -1;
-        
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
+
     }
 }
