abf5035a024bf12cdc446b05a35e3dd5cf222c61
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-06-11 22:40:32 +0530

    Time: 2 ms (68.17%) | Memory: 42.3 MB (86.29%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,131 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        
+ 
+
+        int m = grid[0].length;
+        int n = grid.length;
+
+        if(n == 1 && m == 1)
+        {
+            if(grid[n-1][m-1] == 1)
+            {
+                return -1;
+            }
+
+            if(grid[n-1][m-1] == 0)
+            {
+                return 0;
+            }
+
+            // if(grid[n-1][m-1] == 0)
+        }
+
+        boolean[][] vis = new boolean[n][m];
+        Queue<int[]> q = new LinkedList<>();
+
+        int freshOrg = 0;
+
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<m;j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.add(new int[]{i,j});
+                }
+
+                if(grid[i][j] == 1)
+                {
+                    freshOrg++;
+                }
+
+            }
+        }
+
+        if(freshOrg == 0)
+        {
+            return 0;
+        }
+
+        
+
+        // vis[q.peek()[0]][q.peek()[1]] = true;
+        int time = 0;
+        // boolean flag = false;
+
+        while(!q.isEmpty())
+        {
+
+            int size = q.size();
+            boolean flag = false;
+
+            for(int i = 0;i<size;i++)
+            {
+                int[] currArr = q.remove();
+
+                int row = currArr[0];
+                int col = currArr[1];
+
+                if(row > 0 && grid[row-1][col] == 1 && vis[row-1][col] == false)
+                {
+                    q.add(new int[]{row-1,col});
+                    vis[row-1][col] = true;
+                    freshOrg--;
+                    flag = true;
+                    // grid[row-1][col] = 2;
+                }
+
+                if(row < n-1 && grid[row+1][col] == 1 && vis[row+1][col] == false)
+                {
+                    q.add(new int[]{row+1,col});
+                    vis[row+1][col] = true;
+                    freshOrg--;
+                    flag =true;
+                    //  grid[row-1][col] = 2;
+                }
+
+                if(col > 0 && grid[row][col-1] == 1 && vis[row][col-1] == false)
+                {
+                    q.add(new int[]{row,col-1});
+                    vis[row][col-1] = true;
+                    freshOrg--;
+                    flag =true;
+                    //  grid[row-1][col] = 2;
+                }
+
+                if(col < m-1 && grid[row][col+1] == 1 && vis[row][col+1] == false)
+                {
+                    q.add(new int[]{row,col+1});
+                    vis[row][col+1] = true;
+                    freshOrg--;
+                    flag =true;
+                    //  grid[row-1][col] = 2;
+                }
+            }
+            if(flag)
+            {
+                time++;
+            }
+        }
+
+
+        // for(int i = 0;i<n;i++)
+        // {
+        //     for(int j = 0;j<m;j++)
+        //     {
+        //         if(grid[i][j] == 1 && vis[i][j] == false)
+        //         {
+        //             return -1;
+        //         }
+        //     }
+        // }
+
+
+        return freshOrg == 0?time:-1;
+
+
+        
+    }
+}
