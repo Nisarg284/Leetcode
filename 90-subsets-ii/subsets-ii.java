98aba2b3bc5d838f023501bc7b2adb2db2077d6a
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-06-15 14:04:24 +0530

    Time: 19 ms (6.82%) | Memory: 43.9 MB (11.96%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+
+     public static void helper(int[]nums,int idx,List<List<Integer>>ans,List<Integer> subAns)
+    {
+        // if(power <= 0)
+        // {
+        //     return;
+        // }
+
+        ans.add(new ArrayList<>(subAns));
+
+        for(int i = idx;i<nums.length;i++)
+        {
+            System.out.print(nums[i] +" ");
+           
+            if(i > idx && nums[i] == nums[i-1])
+            {
+                continue;
+            }
+            subAns.add(nums[i]);
+            helper(nums, i+1, ans, subAns);
+
+            
+            subAns.removeLast();
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        if(nums.length == 0)
+        {
+            return new ArrayList<>();
+        }
+
+
+        int n = nums.length;
+        Arrays.sort(nums);
+        // int power = (int)Math.pow(2,n);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> subAns = new ArrayList<>();
+
+        helper(nums,0,ans,subAns);
+
+//        ans.add(new ArrayList<>());
+
+        return ans;
+        
+    }
+}
