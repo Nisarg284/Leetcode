fd65ac1467148284eb4059b92d181bd5e3c29107
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-07-05 11:53:43 +0530

    Time: 283 ms (6.50%) | Memory: 45.5 MB (5.82%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -0,0 +1,60 @@
+class Solution {
+
+    public static void helper(int k,StringBuilder sb)
+    {
+        if(sb.length() > k)
+        {
+            return;
+        }
+
+        int size = sb.length();
+        for(int i = 0;i<size;i++)
+        {
+            char curr = sb.charAt(i);
+            char next = (char)(curr+1);
+
+            if(curr == 'z')
+            {
+                next = 'a';
+            }
+
+            System.out.print(curr+" --> "+next+" ,");
+
+            // if(sb.length() > k)
+            // {
+            //     return;
+            // }
+            if(curr == 'z')
+            {
+                sb.append('a');
+            }else{
+                // System.out.println((char)(curr+1));
+                sb.append((char)(curr+1));
+            }
+        }
+
+        helper(k,sb);
+    }
+    public char kthCharacter(int k) {
+
+        if(k==1)
+        {
+            return 'a';
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('a');
+
+        helper(k,sb);
+
+        return sb.charAt(k-1);
+        
+    }
+}
+
+//                                           a
+// a-> b =                                   ab
+// a->b,b->c =                               abbc
+// a->b,b->c,b->c,c->d =                     abbcbccd
+// a->b,b->c,b->c,c->d,b->c,c->d,c->d,d->e = abbcbccdbccdcdbe 
+
