230a1f3da7842c20d54baaab22395726169bde4b
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-07-08 22:53:43 +0530

    Time: 200 ms (7.73%) | Memory: 77.7 MB (29.46%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,64 @@
+class Solution {
+
+
+    public static void dfsHelper(int start, HashMap<Integer,List<Integer>> outgoing,HashMap<Integer,List<Integer>> incoming,int[]ans,HashSet<Integer> vis)
+    {
+        vis.add(start);
+
+        List<Integer> out = outgoing.get(start);
+
+        if(out != null)
+        {
+            for(int i : out)
+            {
+                if(!vis.contains(i))
+                {
+                    ans[0]++;
+                    dfsHelper(i,outgoing,incoming,ans,vis);
+                }
+            }
+        }
+
+        List<Integer> in = incoming.get(start);
+        if(in != null)
+        {
+            for(int i : in)
+            {
+                if(!vis.contains(i))
+                {
+                    dfsHelper(i,outgoing,incoming,ans,vis);
+                }
+            }
+        }
+
+    }
+    public int minReorder(int n, int[][] connections) {
+
+        int len = connections.length;
+        
+
+        HashMap<Integer,List<Integer>> outgoing = new HashMap<>();
+        HashMap<Integer,List<Integer>> incoming = new HashMap<>();
+
+        for(int i = 0;i<len;i++)
+        {
+            int src = connections[i][0];
+            int dest = connections[i][1];
+
+            outgoing.computeIfAbsent(src,k -> new ArrayList<>()).add(dest);
+            incoming.computeIfAbsent(dest,k -> new ArrayList<>()).add(src);
+        }
+
+        System.out.println(outgoing);
+        System.out.println(incoming);
+
+
+        int[]ans = new int[1];
+        int start = 0;
+
+        dfsHelper(start,outgoing,incoming,ans,new HashSet<>());
+
+        return ans[0];
+        
+    }
+}
