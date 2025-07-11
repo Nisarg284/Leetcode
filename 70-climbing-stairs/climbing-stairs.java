278d909fb0b60419bc6fffe2fc41f3bab8864224
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-07-11 14:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (18.10%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -16,32 +16,68 @@ class Solution {
     //     return ans;
 
     // }
-    public int climbStairs(int n) {
+    // public int climbStairs(int n) {
 
-    //   HashMap<Integer,Integer> hm = new HashMap<>();
+    // //   HashMap<Integer,Integer> hm = new HashMap<>();
 
-    // if(n == 1 || n == 2)
+    // // if(n == 1 || n == 2)
+    // // {
+    // //     return n;
+    // // }
+
+
+    // int[] dp = new int[n+1];
+
+    // dp[0] = 1;
+    // dp[1] = 1;
+
+    // for(int i = 2;i<dp.length;i++)
     // {
-    //     return n;
+    //     dp[i] = dp[i-1] + dp[i-2];
     // }
 
-
-    int[] dp = new int[n+1];
-
-    dp[0] = 1;
-    dp[1] = 1;
-
-    for(int i = 2;i<dp.length;i++)
-    {
-        dp[i] = dp[i-1] + dp[i-2];
-    }
-
-    return dp[dp.length-1];
-    //   return climbStairs(n-1) + climbStairs(n-2);
+    // return dp[dp.length-1];
+    // //   return climbStairs(n-1) + climbStairs(n-2);
 
 
         
+    // }
+
+
+    public static int helper(int n,int[]dp)
+    {
+        if(n == 0 || n == 1)
+        {
+            return 1;
+        }
+
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+
+        dp[n] = helper(n-1,dp) + helper(n-2,dp);
+
+        return dp[n];
     }
+
+    public int climbStairs(int n){
+        // if(n == 1 || n == 2)
+        // {
+        //     return n;
+        // }
+
+        int[] dp = new int[n+1];
+
+        for(int i = 0;i<=n;i++)
+        {
+            dp[i] = -1;
+        }
+        
+
+        return helper(n,dp);
+    }
+
 }
 
 // 5 = 1 1 1 1 1
