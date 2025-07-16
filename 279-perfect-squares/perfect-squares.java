eba524fb1bb0d6b060bf271fbb6229f6e54a4eb4
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-07-16 21:53:12 +0530

    Time: 135 ms (18.32%) | Memory: 47.5 MB (11.68%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,40 +1,45 @@
 class Solution {
-
-    public static int helper(int n,int[]dp)
-    {
-        if(n==0)
-        {
-            return 0;
-        }
-
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
-
-
-        int min = Integer.MAX_VALUE;
-
-        for(int i = 1;i*i<=n;i++)
-        {
-            int res = 1 + helper(n-i*i,dp);
-            min = Math.min(res,min);
-        }
-        return dp[n] = min;
-    }
     public int numSquares(int n) {
-        
-        if(n==1)
+
+        Queue<Integer> q = new LinkedList<>();
+
+        Set<Integer> hs = new HashSet<>();
+        hs.add(n);
+        q.add(n);
+
+
+
+        int level = 0;
+
+        while(!q.isEmpty())
         {
-            return 1;
+            int size = q.size();
+
+
+            for(int j = 0;j<size;j++)
+            {
+                n = q.remove();
+                if( n == 0)
+                {
+                    return level;
+                }
+                for(int i = 1;i*i<=n;i++)
+                {
+                    int newEle = n-i*i;
+                    if(!hs.contains(newEle))
+                    {
+                        q.add(newEle);
+                        hs.add(newEle);
+                    }
+                    
+                }
+            }
+            level++;
         }
 
-        int[]dp = new int[n+1];
-        Arrays.fill(dp,-1);
-
-        int steps = 0;
-
-        return helper(n,dp);
+        return level;
         
     }
 }
+
+
