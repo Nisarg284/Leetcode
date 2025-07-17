2b23925fd0b7e9214c4c5d97a5463b6032f2d69e
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-07-17 21:16:11 +0530

    Time: 61 ms (43.97%) | Memory: 59 MB (18.35%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -0,0 +1,86 @@
+class Solution {
+
+    public static long findMin(int[] time,int n)
+    {
+        long min = Long.MAX_VALUE;
+        for(int i = 0;i<n;i++)
+        {
+            min = Math.min(min,time[i]);
+        }
+
+        return min;
+    }
+
+    public static boolean isValid(int[]arr,int totalTrips,long mid,int n)
+    {
+        long trips = 0;
+
+        for(int i = 0;i<n;i++)
+        {
+            trips += mid / arr[i];
+        }
+
+        return trips >= totalTrips ? true : false;
+    }
+
+    public long minimumTime(int[] time, int totalTrips) {
+
+        int n = time.length;
+
+        long start = 1;
+        long end = findMin(time,n) * totalTrips;
+
+
+        while(start < end)
+        {
+            long mid =  start + (end - start) /2;
+            if(isValid(time,totalTrips,mid,n))
+            {
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        System.out.println("start: "+start+" end: "+end);
+
+        return start;
+
+
+
+// Brute force Approach
+
+        // int n = time.length;
+        // // int count = 0;
+        // int ti = 1;
+
+        // while(true)
+        // {
+        //     int trips = 0;
+        //     for(int i = 0;i<n;i++)
+        //     {
+        //         // if(totalTrips <= 0)
+        //         // {
+        //         //     return ti;
+        //         // }
+        //         trips += ti/ time[i];
+        //         if(trips>=totalTrips)
+        //         {
+        //             break;
+        //         }
+        //         System.out.print(trips+" ");
+        //     }
+        //     if(trips>=totalTrips)
+        //     {
+        //         break;
+        //     }
+        //     System.out.println();
+        //     ti++;
+        // }
+
+        // return ti;
+
+        
+        
+    }
+}
