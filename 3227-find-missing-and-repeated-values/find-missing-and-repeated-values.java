fddbf0496f386b5fece7930fb7ba14001f633584
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-07-30 21:45:02 +0530

    Time: 5 ms (38.18%) | Memory: 45.8 MB (14.87%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,37 +1,48 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
 
-       HashSet<Integer> hs = new HashSet<>();
-        int max = Integer.MIN_VALUE;
+        HashSet<Integer> hs = new HashSet<>();
 
-        int ans[] = new int[2];
+        int n = grid.length;
+        int m =grid[0].length;
 
-        for(int i=0;i<grid.length;i++)
+        int [] ans = new int[2];
+
+        boolean flag = false;
+
+        for(int i = 0;i<n;i++)
         {
-            for(int j=0;j<grid[0].length;j++)
+            for(int j = 0;j<m;j++)
             {
                 if(hs.contains(grid[i][j]))
                 {
                     ans[0] = grid[i][j];
+                    // flag = true;
+                    // break;
                 }
-                hs.add(grid[i][j]); 
+
+                hs.add(grid[i][j]);
+
             }
+
+
+            // if(flag == true)
+            // {
+            //     break;
+            // }
         }
 
-        int missing = -1;
-        int i =1;
-        while(true)
+
+        for(int i = 1;i<=n*n;i++)
         {
             if(!hs.contains(i))
             {
                 ans[1] = i;
                 break;
             }
-            i++;
         }
 
         return ans;
-         
         
     }
 }
