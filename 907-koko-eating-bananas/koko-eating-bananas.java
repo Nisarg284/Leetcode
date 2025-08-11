b32b5b7dfa8537a25097e391283ac74e5e9b8334
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-08-11 12:37:50 +0530

    Time: 12 ms (65.38%) | Memory: 44.9 MB (89.10%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,51 +1,104 @@
 class Solution {
 
-    public static int findMax(int[]arr,int n)
+    // public static int findMax(int[]arr,int n)
+    // {
+    //     int max = arr[0];
+    //     for(int i : arr)
+    //     {
+    //         max = Math.max(max,i);
+    //     }
+
+    //     return max;
+    // }
+
+    // public static boolean canEat(int[]arr,int n,int mid,int h)
+    // {
+    //     int hrs = 0;
+
+    //     for(int i:arr)
+    //     {
+    //         hrs += (i + mid - 1) / mid;
+    //     }
+
+    //     // System.out.println(hrs+" mid: "+mid);
+
+    //     return hrs <= h;
+    // }
+    // public int minEatingSpeed(int[] piles, int h) {
+
+    //     int n = piles.length;
+
+    //     int start = 1;
+    //     int end  = findMax(piles,n);
+
+
+    //     while(start < end)
+    //     {
+    //         int mid = (start + end) / 2;
+
+    //         if(canEat(piles,n,mid,h))
+    //         {
+    //             end = mid;
+    //         }else{
+    //             start = mid + 1;
+    //         }
+    //     }
+
+
+    //     return start;
+        
+    // }
+
+
+      public static int findMax(int[]arr)
     {
-        int max = arr[0];
-        for(int i : arr)
+        int max = Integer.MIN_VALUE;
+        for (int i : arr)
         {
-            max = Math.max(max,i);
+            max = Math.max(i,max);
         }
 
         return max;
     }
 
-    public static boolean canEat(int[]arr,int n,int mid,int h)
+    public static boolean isValid(int[]arr,int mid,int hrs)
     {
-        int hrs = 0;
+        int time = 0;
 
-        for(int i:arr)
+        for(int i = 0 ;i<arr.length;i++)
         {
-            hrs += (i + mid - 1) / mid;
+//            time += (arr[i] / mid)+1;
+            if(arr[i] % mid == 0)
+            {
+                time += (arr[i]/mid);
+            }else{
+                time += (arr[i]/mid)+1;
+            }
+
         }
 
-        // System.out.println(hrs+" mid: "+mid);
-
-        return hrs <= h;
+        return time <= hrs;
     }
-    public int minEatingSpeed(int[] piles, int h) {
 
+    public static int minEatingSpeed(int[]piles,int hrs)
+    {
         int n = piles.length;
 
-        int start = 1;
-        int end  = findMax(piles,n);
+        int i = 1;
+        int j = findMax(piles);
 
-
-        while(start < end)
+        while( i < j)
         {
-            int mid = (start + end) / 2;
+            int mid = (i + j)/2;
 
-            if(canEat(piles,n,mid,h))
+            if(isValid(piles,mid,hrs))
             {
-                end = mid;
+                j = mid;
             }else{
-                start = mid + 1;
+                i = mid+1;
             }
         }
 
-
-        return start;
-        
+        return i;
     }
 }
