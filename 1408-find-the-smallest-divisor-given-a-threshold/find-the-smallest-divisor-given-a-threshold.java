6adeb4706447729f6cd92510d613277482e5feb2
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-08-11 20:11:14 +0530

    Time: 8 ms (73.47%) | Memory: 47.9 MB (90.81%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -6,16 +6,14 @@ class Solution {
 
         for(int i = 0;i<n;i++)
         {
-            if(nums[i] % mid == 0)
-            {
-                sum+=(nums[i] / mid);
-            }else{
-                sum += (nums[i]/mid)+1;
-            }
-
-            // sum += (nums[i] + mid - 1) / mid;
+            // if(nums[i] % mid == 0)
+            // {
+            //     sum+=(nums[i] / mid);
+            // }else{
+            //     sum += (nums[i]/mid)+1;
+            // }
+            sum += (nums[i] + mid - 1) / mid;
         }
-
         // System.out.println(sum);
 
         return sum <= threshold;
