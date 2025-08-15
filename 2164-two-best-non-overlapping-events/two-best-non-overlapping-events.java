50fffb0f997c67c1f64f5f5f0ed697298a9c068e
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-08-16 01:20:41 +0530

    Time: 40 ms (90.00%) | Memory: 102.3 MB (89.69%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,71 +1,33 @@
 class Solution {
-
-    public static int binarySearch(int[][]arr,int target,int n)
-    {
-        int i = 0;
-        int j = n-1;
-
-        int idx = -1;
-
-
-        while(i<=j)
-        {
-            int mid = (i+j)/2;
-            // System.out.println("mid : "+ mid);
-
-            if(arr[mid][1] < target)
-            {
-                idx = mid;
-                i = mid+1;
-            }else{
-                j = mid - 1;
-            }
-        }
-
-        // System.out.println("idx : "+idx+" , target : "+target);
-        return idx;
-    }
     public int maxTwoEvents(int[][] events) {
 
         int n = events.length;
 
-        Arrays.sort(events,(a,b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(events,(a,b) -> a[0] - b[0]);
 
-        int[] dp = new int[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
 
-        dp[0] = events[0][2];
+        int ans = 0;
+        int maxVal = 0;
 
-        for(int i = 1;i<n;i++)
+        for(int [] event : events)
         {
-            dp[i] = Math.max(dp[i-1],events[i][2]);
+            int start = event[0];
+            int end = event[1];
+            int value = event[2];
+
+            while(!pq.isEmpty() && pq.peek()[1] < start)
+            {
+                maxVal = Math.max(maxVal,pq.remove()[2]);
+            }
+
+            pq.add(event);
+
+            ans = Math.max(ans,maxVal + value);
+
         }
 
-        int ans = dp[n-1];
-
-        for(int i = 1;i<n;i++)
-        {
-           int start = events[i][0];
-           int currValue = events[i][2];
-           int idx = binarySearch(events,start,n);
-
-
-           if(idx != -1)
-           {
-            // System.out.println(" currValue "+currValue+" + dp[idx] "+dp[idx] +" = "+ (dp[idx] + currValue));
-                currValue += dp[idx];
-           } 
-
-           int include = currValue;
-           int exclude = ans;
-
-           ans = Math.max(include,exclude);
-        }
-
-
-        // for(int i = 0;i<n;i++)
-        // {
-        //     System.out.println("i"+(i+1)+" : "+dp[i]);
-        // }
-        return ans;        
+        return ans;
+        
     }
 }
