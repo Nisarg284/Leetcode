b5faba8588fe384a38df54ed05cf2e757b79d3ab
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-08-28 22:44:02 +0530

    Time: 41 ms (60.72%) | Memory: 45.2 MB (66.79%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,72 +1,58 @@
 class Solution {
-
-    // public static void helper(int row,int col,PriorityQueue<int[]>)
     public int minimumEffortPath(int[][] heights) {
 
         int n = heights.length;
         int m = heights[0].length;
 
-        int[][] distance = new int[n][m];
+        int[][] effort = new int[n][m];
 
-        for(int i = 0;i<n;i++)
+        for(int i[] : effort)
         {
-            for(int j= 0;j<m;j++)
-            {
-                if( i== 0 && j == 0)
-                {
-                    continue;
-                }
-
-                distance[i][j] = Integer.MAX_VALUE;
-            }
+            Arrays.fill(i,Integer.MAX_VALUE);
         }
+
+        effort[0][0] = 0;
 
         int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2]-b[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2] - b[2]);
 
         pq.add(new int[]{0,0,0});
 
         while(!pq.isEmpty())
         {
-            int[] cellData = pq.remove();
-            int row = cellData[0];
-            int col = cellData[1];
-            int srcHeight = cellData[2];
+            int[] curr = pq.remove();
+            int row = curr[0];
+            int col = curr[1];
+            int currCost = curr[2];
 
             if(row == n-1 && col == m-1)
             {
-                return srcHeight;
+                return currCost;
             }
 
-            for(int[] d : dir)
-            {
+
+            for(int d[] : dir){
                 int newRow = row + d[0];
                 int newCol = col + d[1];
 
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m)
+                if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < m)
                 {
+                    int srcHeight = heights[row][col];
                     int destHeight = heights[newRow][newCol];
-                    int newDiff = Math.abs(heights[row][col] - destHeight);
-                    int newHeight = Math.max(srcHeight,newDiff);
+                    int absDiff = Math.abs(srcHeight - destHeight);
+                    int maxDiff = Math.max(absDiff,currCost);
 
-                    if( newHeight < distance[newRow][newCol])
+                    if(maxDiff < effort[newRow][newCol])
                     {
-                        distance[newRow][newCol] = newHeight;
-                        pq.add(new int[]{newRow,newCol,newHeight});
+                        effort[newRow][newCol] = maxDiff;
+                        pq.add(new int[]{newRow,newCol,maxDiff});
                     }
                 }
             }
-
-
         }
 
-        return distance[n-1][m-1];
-
-       
-
-
-
+        return effort[n-1][m-1];
         
     }
 }
