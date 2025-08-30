a6419edc863607a9a227144e577591aca3cda820
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-08-30 15:53:49 +0530

    Time: 17 ms (73.63%) | Memory: 44.3 MB (82.94%) - LeetSync

---
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int shortestPath(int[][] grid, int k) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        // if (k >= n + m - 2) {
+        //     return n + m - 2;
+        // }
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int[][] minCost = new int[n][m];
+
+        for (int[] arr : minCost) {
+            Arrays.fill(arr, -1);
+        }
+        
+
+        // if(grid[0][0] == 1)
+        // {
+        //     minCost[0][0] = k-1;
+        // }else{
+        //     minCost[0][0] = k;
+        // }
+
+        minCost[0][0] = k;
+
+        q.add(new int[]{0,0,0,minCost[0][0]});
+
+        int[][] dir = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.remove();
+                int row = curr[0];
+                int col = curr[1];
+                int steps = curr[2];
+                int leftK = curr[3];
+
+                if (row == n - 1 && col == m - 1) {
+                    return steps;
+                }
+
+                for (int[] d : dir) {
+                    int newRow = row + d[0];
+                    int newCol = col + d[1];
+
+                    if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m) {
+                        int newK = leftK - grid[newRow][newCol];
+
+                        if(newK >= 0 && newK > minCost[newRow][newCol])
+                        {
+                            q.add(new int[]{newRow,newCol,steps+1,newK});
+                            minCost[newRow][newCol] = newK;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return -1;
+
+    }
+}
