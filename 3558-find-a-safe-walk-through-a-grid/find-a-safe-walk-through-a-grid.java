9af44d0a75ecce1526325ff72e6ec8791a54460d
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-08-30 17:14:24 +0530

    Time: 22 ms (56.39%) | Memory: 45.7 MB (23.68%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -1,66 +1,60 @@
-
 class Solution {
-
-    public static boolean dfsHelper(List<List<Integer>> grid,int row,int col,int n ,int m,boolean[][][]vis,int health,int[][] dir)
-    {
-        if(row < 0 || col < 0 || row >= n || col >= m || health <= 0 || vis[row][col][health] == true)
-        {
-            return false;
-        }
-
-        vis[row][col][health] = true;
-
-        if(row == n-1 && col == m-1 )
-        {
-
-            if(grid.get(row).get(col) == 1)
-            {
-                health -=1;
-            }
-
-            return health >= 1;
-            
-        }
-
-        boolean curr = false;
-        
-
-       for(int[] d : dir)
-       {
-            int newRow = row + d[0];
-            int newCol = col + d[1];
-            
-            // vis[row][col][health] = true;
-            if(newRow >=0 && newCol >= 0 && newRow <n && newCol < m )
-            {
-                // vis[row][col] = true;
-                // if(grid.get(row).get(col)  == 1)
-                // {
-                //     curr =  curr || dfsHelper(grid,newRow,newCol,n,m,vis,health - 1,dir);
-                // }else{
-                //     curr =  curr || dfsHelper(grid,newRow,newCol,n,m,vis,health,dir);
-                // }
-
-                curr = curr || dfsHelper(grid,newRow,newCol,n,m,vis,health - grid.get(row).get(col),dir);
-                // vis[row][col] =false;
-            }
-            //  vis[row][col] =false;
-            
-       }
-
-       return curr;
-        
-    }
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        
 
         int n = grid.size();
         int m = grid.get(0).size();
 
-        boolean[][][] vis = new boolean[n][m][health+1];
+
+        Queue<int[]> q = new LinkedList<>();
+        int[][] minCost = new int[n][m];
+
+        for(int[] arr : minCost)
+        {
+            Arrays.fill(arr,-1);
+        }
+
+        minCost[0][0] = health - grid.get(0).get(0);
+        q.add(new int[]{0,0,minCost[0][0]});
+
+
         int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
 
-        return dfsHelper(grid,0,0,n,m,vis,health,dir);
-        
+
+        while(!q.isEmpty())
+        {
+            int[] curr = q.remove();
+            int row = curr[0];
+            int col = curr[1];
+            int cost = curr[2];
+
+            if(row == n-1 && col == m-1)
+            {
+                return true;
+            }
+
+
+            for(int[] d : dir)
+            {
+                int newRow = row + d[0];
+                int newCol = col + d[1];
+
+                if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < m)
+                {
+                    int newCost = cost - grid.get(newRow).get(newCol);
+
+                    if(newCost > 0 && newCost > minCost[newRow][newCol])
+                    {
+                        
+                        q.add(new int[]{newRow,newCol,newCost});
+                        minCost[newRow][newCol] = newCost;
+                    }
+                }
+            }
+
+
+        }
+
+        return minCost[n-1][m-1] != -1;
     }
 }
-
