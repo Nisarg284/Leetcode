354a0f7023c553248db9b850ebd20ecd606249bc
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-09-15 21:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (46.65%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,48 +1,42 @@
 class Solution {
 
-    public static int helper(int[] nums,int[]dp,int n)
+    public static int helper(int idx,int[]nums,int[]dp)
     {
-        if(n < 0)
+        if(idx < 0)
         {
             return 0;
         }
 
-        if(dp[n] != -1)
+        if(dp[idx] != -1)
         {
-            return dp[n];
+            return dp[idx];
         }
 
-        int steal = nums[n] + helper(nums,dp,n-2);
-        int skip = helper(nums,dp,n-1);
+        int steal = nums[idx] + helper(idx-2,nums,dp);
+        int skip = helper(idx-1,nums,dp);
 
-        dp[n] = Math.max(steal,skip);
-
-        return dp[n];
+        dp[idx] = Math.max(steal,skip);
+        return dp[idx];
     }
     public int rob(int[] nums) {
 
+
         int n = nums.length;
-
-        int[]dp = new int[n];
-        Arrays.fill(dp,-1);
-
-        // return helper(nums,dp,n-1);
-
-        if( n == 1 )
+        if(n == 1)
         {
             return nums[0];
         }
 
+        int[] dp = new int[n];
 
+        Arrays.fill(dp,-1);
+
+        int idx = n-1;
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        dp[1] = Math.max(nums[1],nums[0]);
 
-        for(int i = 2;i<n;i++)
-        {
-            dp[i] = Math.max(nums[i] + dp[i-2],dp[i-1]);
-        }
-
-        return dp[n-1];
+        return helper(idx,nums,dp);
+        
         
     }
 }
