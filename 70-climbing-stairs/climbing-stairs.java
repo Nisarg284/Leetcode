d24878714cd15b01f7b1844381f6a9a7e4d82d7b
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-09-15 11:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (39.08%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,90 +1,33 @@
 class Solution {
 
-    // public static int helper(int n,HashMap<Integer,Integer>hm)
-    // {
-    //     // Base Case:
-    //     if(n==1 || n== 2)
-    //     {
-    //         return n;
-    //     }
-
-    //     if(!hm.containsKey(n))
-    //     {
-    //         hm.put(n,helper(n-1,hm)+helper(n-2,hm)); 
-    //     }
-    //     int ans = hm.get(n);
-    //     return ans;
-
-    // }
-    // public int climbStairs(int n) {
-
-    // //   HashMap<Integer,Integer> hm = new HashMap<>();
-
-    // // if(n == 1 || n == 2)
-    // // {
-    // //     return n;
-    // // }
-
-
-    // int[] dp = new int[n+1];
-
-    // dp[0] = 1;
-    // dp[1] = 1;
-
-    // for(int i = 2;i<dp.length;i++)
-    // {
-    //     dp[i] = dp[i-1] + dp[i-2];
-    // }
-
-    // return dp[dp.length-1];
-    // //   return climbStairs(n-1) + climbStairs(n-2);
-
-
-        
-    // }
-
-
-    public static int helper(int n,int[]dp)
+    public static int helper(int n,int[]memo)
     {
-        if(n == 0 || n == 1)
+        if(n < 0)
         {
-            return 1;
+            return 0;
         }
 
-        if(dp[n] != -1)
+        if(memo[n] != -1)
         {
-            return dp[n];
+            return memo[n];
         }
 
-        dp[n] = helper(n-1,dp) + helper(n-2,dp);
+        int oneStep = helper(n-1,memo);
+        int twoStep = helper(n-2,memo);
 
-        return dp[n];
-    }
-
-    public int climbStairs(int n){
-        // if(n == 1 || n == 2)
-        // {
-        //     return n;
-        // }
-
-        int[] dp = new int[n+1];
-
-        for(int i = 0;i<=n;i++)
-        {
-            dp[i] = -1;
-        }
         
 
-        return helper(n,dp);
+        return memo[n] = oneStep + twoStep;
     }
+    public int climbStairs(int n) {
 
+
+        int[] memo = new int[n+1];
+        Arrays.fill(memo,-1);
+        memo[0] = 1;
+        memo[1] = 1;
+
+        return helper(n,memo);
+        
+    }
 }
-
-// 5 = 1 1 1 1 1
-//     1 2 2
-//     1 2 1 1
-//     2 1 1 1
-//     2 2 1
-//     2 1 2
-//     1 1 1 2
-//     1 1 2 1
