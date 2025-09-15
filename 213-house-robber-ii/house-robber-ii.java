576407aebdb1a3f7350c40ab4da581660fd1db47
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-09-15 21:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (18.60%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,38 +1,28 @@
 class Solution {
 
-    public static int helper(int[]dp,int[]nums,int n,int stop)
+    public static int helper(int idx,int[]nums,int[]dp,int stop)
     {
-        if(n < stop)
+        if(idx < stop)
         {
             return 0;
         }
 
-        
-
-        // if(flag == true)
-        // {
-        //     return nums[n];
-        // }
-
-
-        if(dp[n] != -1)
+        if(idx == stop)
         {
-            return dp[n];
+            return nums[idx];
         }
 
-        int steal = nums[n] + helper(dp,nums,n-2,stop);
-        int skip = helper(dp,nums,n-1,stop);
+        if(dp[idx] != -1)
+        {
+            return dp[idx];
+        }
 
-        // if(flag && n == nums.length - 1) {
-        //     dp[n] = skip;
-        // } else {
-        //     dp[n] = Math.max(steal, skip);
-        // }
+        int steal = nums[idx] + helper(idx-2,nums,dp,stop);
+        int skip = helper(idx-1,nums,dp,stop);
 
-        return dp[n] = Math.max(steal,skip);
+        return dp[idx] = Math.max(steal,skip);
     }
     public int rob(int[] nums) {
-
 
         int n = nums.length;
 
@@ -40,16 +30,16 @@ class Solution {
         {
             return nums[0];
         }
-        int[] dp = new int[n];
+        int[]dp = new int[n];
+
+        // int start = n-1;
+        int idx = n-1;
+        Arrays.fill(dp,-1);
+        int stealFromFirst = helper(idx,nums,dp,1);
 
         Arrays.fill(dp,-1);
+        int stealFromSecond = helper(idx-1,nums,dp,0);
 
-        int ans1 = helper(dp,nums,n-1,1);
-        Arrays.fill(dp,-1);
-
-        int ans2 = helper(dp,nums,n-2,0);
-
-        return Math.max(ans1,ans2);
-        
+        return Math.max(stealFromFirst,stealFromSecond);
     }
 }
