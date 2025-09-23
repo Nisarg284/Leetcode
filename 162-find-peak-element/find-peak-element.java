9186a8f817f3cd98f4ca50df5545dd8ac8a3e983
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-09-23 19:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (7.18%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,50 +1,26 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        // int left = 0, right = nums.length - 1;
+    public int findPeakElement(int[] arr) {
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
 
-        // while (left < right) {
-        //     int mid = (left + right) / 2;
-
-        //     if (nums[mid] > nums[mid + 1]) {
-        //         right = mid; // Peak is on the left or at mid
-        //     } else {
-        //         left = mid + 1; // Peak is on the right
-        //     }
-        // }
-        // return left; // Left and right converge at the peak index
-
-
-        int n = nums.length;
-        int i = 0;
-        int j = n-1;
-
-        if( n < 3)
+        while (start < end)
         {
-            if(nums[i] > nums[j])
-            {
-                return i;
-            }else{
-                return j;
-            }
-        }
+            int mid = (start + end) /2;
 
-        while(i<=j)
-        {
-            int mid = (i + j)/2;
-
-            if((mid == 0 && nums[mid] > nums[mid+1]) || (mid == n-1 && nums[mid] > nums[mid-1]) || ((mid<n-1 && mid>0) && nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]))
+            if(mid - 1 >= 0 && mid+1 < n && arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
             {
                 return mid;
             }
 
-            if(nums[mid]<= nums[mid+1])
+            if(arr[mid] < arr[mid+1])
             {
-                i = mid+1;
+                start = mid+1;
             }else{
-                j = mid-1;
+                end = mid-1;
             }
         }
 
-        return -1;
+        return start;
     }
 }
