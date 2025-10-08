b6ddee45039e24e6f4503a8e9a17dd9fc7245b04
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-10-08 22:08:47 +0530

    Time: 1 ms (99.98%) | Memory: 45.5 MB (41.01%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,75 +1,43 @@
 class Solution {
+
+    public static int helper(int row,int col,int[][]dp,int[][]matrix,int n)
+    {
+        if(col < 0 || col >=n)
+        {
+            return Integer.MAX_VALUE;
+        }
+
+        if(row == 0)
+        {
+            return matrix[row][col];
+        }
+
+        if(dp[row][col] != Integer.MAX_VALUE)
+        {
+            return dp[row][col];
+        }
+
+        int top = helper(row-1,col,dp,matrix,n);
+        int leftTopDiagonal = helper(row-1,col-1,dp,matrix,n);
+        int rightTopDiagonal = helper(row-1,col+1,dp,matrix,n);
+
+        return dp[row][col] = matrix[row][col] + Math.min(top,Math.min(leftTopDiagonal,rightTopDiagonal));
+    }
     public int minFallingPathSum(int[][] matrix) {
 
         int n = matrix.length;
 
-        int[][]dp = new int[n][n];
-
-        for(int[]arr : dp)
+        int[][] dp = new int[n][n];
+        for(int arr[] : dp)
         {
             Arrays.fill(arr,Integer.MAX_VALUE);
         }
 
-        dp[0][0] = matrix[0][0];
-
-        
-
-        // filling first col
-        // for(int i = 1;i<n;i++)
-        // {
-        //     dp[i][0] = matrix[i][0] + dp[i-1][0];
-        // }
-
-        // filling first row
-        // int minVal = Arrays.stream(matrix[0]).min().getAsInt();
-
-        // Arrays.fill(matrix[0],minVal);
-        for(int i = 0;i<n;i++)
-        {
-            dp[0][i] = matrix[0][i];
-        }
-
-        // System.out.println(minVal);
-
-        // for(int[]arr : dp)
-        // {
-        //     for(int i : arr)
-        //     {
-        //         System.out.print(i+" ");
-        //     }
-        //     System.out.println();
-        // }
-
-        for(int i = 1;i<n;i++)
-        {
-            for(int j = 0;j<n;j++)
-            {
-                int top = dp[i-1][j];
-                int leftTopDiagonal = Integer.MAX_VALUE;
-                int rightTopDiagonal = Integer.MAX_VALUE;
-
-                if(j > 0)
-                {
-                    leftTopDiagonal = dp[i-1][j-1];
-                }
-
-                if(j < n-1)
-                {
-                    rightTopDiagonal = dp[i-1][j+1];
-                }
-
-                dp[i][j] = matrix[i][j] + Math.min(top,Math.min(leftTopDiagonal,rightTopDiagonal));
-            }
-        }
-
-
         int ans = Integer.MAX_VALUE;
         for(int i = 0;i<n;i++)
         {
-            ans = Math.min(ans,dp[n-1][i]);
+            ans = Math.min(ans,helper(n-1,i,dp,matrix,n));
         }
-
-
 
         return ans;
         
