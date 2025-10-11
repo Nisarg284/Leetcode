ae113aacc96e5f7b78ecc63bacd1f33415ddf745
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-10-11 23:07:46 +0530

    Time: 36 ms (13.28%) | Memory: 46 MB (8.04%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,37 +1,54 @@
 class Solution {
+
+    public static int helper(int idx,int target,int[]coins,int[][]dp)
+    {
+        if(target == 0)
+        {
+            return 0;
+        }
+
+        if(idx < 0)
+        {
+            return Integer.MAX_VALUE;
+        }
+
+        if(dp[idx][target] != -1)
+        {
+            return dp[idx][target];
+        }
+
+        int take = Integer.MAX_VALUE;
+        int skip = helper(idx-1,target,coins,dp);
+        // int takeAndStay = Integer.MAX_VALUE;
+
+        if(coins[idx] <= target)
+        {
+            int stay = helper(idx,target - coins[idx],coins,dp);
+
+            if(stay != Integer.MAX_VALUE)
+            {
+                take = 1 + stay;
+            }
+            // takeAndStay = 1 + helper(idx,target - coins[idx],coins);
+        }
+
+        int findMin = Math.min(take,skip);
+        return dp[idx][target] = findMin;
+    }
     public int coinChange(int[] coins, int amount) {
 
         int n = coins.length;
 
-        int[][] dp = new int[n + 1][amount + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j == 0) {
-                    dp[i][j] = 0;
-                } else if (i == 0) {
-                    dp[i][j] = Integer.MAX_VALUE - 1;
-                }
-            }
+        int[][]dp = new int[n][amount+1];
+
+        for(int[]arr : dp)
+        {
+            Arrays.fill(arr,-1);
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    int accept = dp[i][j - coins[i - 1]];
-                    int reject = dp[i - 1][j];
+        int ans = helper(n-1,amount,coins,dp);
 
-                    if (dp[i][j] != Integer.MAX_VALUE - 1) {
-                        dp[i][j] = Math.min(1 + accept, reject);
-                    } else {
-                        dp[i][j] = reject;
-                    }
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
-
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+        
     }
 }
