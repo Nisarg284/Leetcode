61f75bd1429b7dbee874aaad6c85ded00e2208e0
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-10-12 01:49:14 +0530

    Time: 15 ms (26.65%) | Memory: 53.7 MB (13.18%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,33 +1,46 @@
 class Solution {
+
+    public static int helper(int idx,int target,int[]arr,int[][]dp)
+    {
+        if(target == 0)
+        {
+            return 1;
+        }
+
+        if(idx < 0)
+        {
+            return 0;
+        }
+
+        if(dp[idx][target] != -1)
+        {
+            return dp[idx][target];
+        }
+
+        int take = 0;
+        // int stay = 0;
+        int skip = helper(idx -1,target,arr,dp);
+
+        if(arr[idx] <= target)
+        {
+            // stay = helper(idx,target - arr[idx],arr);
+            take = helper(idx,target - arr[idx],arr,dp);
+        }
+
+        return dp[idx][target] = take + skip;
+    }
     public int change(int amount, int[] coins) {
 
         int n = coins.length;
-        
-        int[][] dp = new int[n+1][amount+1];
 
-        for(int i = 0;i<=n;i++)
+        int[][]dp = new int[n][amount + 1];
+
+        for(int[]arr : dp)
         {
-            dp[i][0] = 1;
+            Arrays.fill(arr,-1);
         }
 
-
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j = 1;j<=amount;j++)
-            {
-                if(coins[i-1]<=j)
-                {
-                    int accept = dp[i][j-coins[i-1]];
-                    int reject = dp[i-1][j];
-
-                    dp[i][j] = accept + reject;
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-
-        return dp[n][amount];
+        return helper(n-1,amount,coins,dp);
         
     }
 }
