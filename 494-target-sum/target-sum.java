2202d8d2a47a4a6e2e08fbe1c6a0360039870e47
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-10-11 23:24:13 +0530

    Time: 508 ms (28.14%) | Memory: 41.5 MB (76.76%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,44 +1,36 @@
 class Solution {
-    public int findTargetSumWays(int[] arr, int diff) {
-        int n = arr.length;
-       int sum = 0;
-       for(int i:arr)
-       {
-        sum+=i;
-       }
 
-       if((sum-diff)%2 == 1 || sum < diff)
-       {
-        return 0;
-       }
+    public static int helper(int idx,int target,int arr[])
+    {
+        // if(idx == 0)
+        // {
+        //     if(arr[idx] == target || target - arr[idx] == 0 )
+        //     {
+        //         return 1;
+        //     }
+        //     return 0;
+        // }
 
-       int s1 = (sum - diff) / 2;
-
-       int[][] dp = new int[n+1][s1+1];
-
-    //    dp[0][0] = 1;
-
-       for(int i=0;i<=n;i++)
-       {
-        dp[i][0] = 1;
-       }
-
-       for(int i = 1;i<=n;i++)
-       {
-        for(int j = 0;j<=s1;j++)
+        if(idx < 0)
         {
-            if(arr[i-1] <= j)
+            if(target == 0)
             {
-                int accept = dp[i-1][j-arr[i-1]];
-                int reject = dp[i-1][j];
-
-                dp[i][j] = accept + reject;
-            }else{
-                dp[i][j] = dp[i-1][j];
+                return 1;
             }
-        }
-       }
 
-       return dp[n][s1];
+            return 0;
+        }
+
+        int plus = helper(idx-1,target - arr[idx],arr);
+        int minus = helper(idx-1,target + arr[idx],arr);
+
+        return plus + minus;
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+
+        int n = nums.length;
+
+        return helper(n-1,target,nums);
+        
     }
 }
