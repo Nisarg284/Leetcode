37c81f2b149d693e7a1505bffebcd85b5d3e2e00
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-10-16 22:48:26 +0530

    Time: 21 ms (27.43%) | Memory: 51.2 MB (67.04%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,13 +1,26 @@
 class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
 
-    public static int helper(int n,int m,String s,String t,int[][]dp)
-    {
-        if(m < 0)
+        int[][] dp = new int[n][m];
+
+        for(int[]arr : dp)
+        {
+            Arrays.fill(arr,-1);
+        }
+
+        return helper(n-1,m-1,s,t,dp);
+    }
+
+    private static int helper(int n, int m, String s, String t, int[][] dp) {
+
+        if(m<0)
         {
             return 1;
         }
 
-        if(n < 0)
+        if(n<0)
         {
             return 0;
         }
@@ -17,29 +30,14 @@ class Solution {
             return dp[n][m];
         }
 
-        int take = helper(n-1,m-1,s,t,dp);
-        int skip = helper(n-1,m,s,t,dp);
-
         if(s.charAt(n) == t.charAt(m))
         {
-            return dp[n][m] = take + skip;
+            return dp[n][m] = helper(n-1,m-1,s,t, dp) + helper(n-1, m, s, t, dp);
         }
 
-        return dp[n][m] = skip;
+        return dp[n][m] = helper(n-1, m, s, t, dp);
     }
-    public int numDistinct(String s, String t) {
 
-        int n = s.length();
-        int m = t.length();
 
-        int[][]dp = new int[n][m];
-
-        for(int[]arr : dp)
-        {
-            Arrays.fill(arr,-1);
-        }
-
-        return helper(n-1,m-1,s,t,dp);
-        
-    }
+    
 }
