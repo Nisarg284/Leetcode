7d146d23289318391ff0dcbd5fcdce6fb540300e
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-10-21 22:33:37 +0530

    Time: 337 ms (17.27%) | Memory: 80 MB (6.68%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,49 +1,44 @@
 class Solution {
+
+    public static int helper(int currIdx,int prevIdx,int n,int[]arr,int[][]dp)
+    {
+        if(currIdx >= n)
+        {
+            return 0;
+        }
+
+        if( prevIdx != -1 && dp[currIdx][prevIdx+1] != Integer.MIN_VALUE)
+        {
+            return dp[currIdx][prevIdx+1];
+        }
+
+        int skip = helper(currIdx+1,prevIdx,n,arr,dp);
+        int accept = Integer.MIN_VALUE;
+
+        // System.out.println("currEle ---> "+ arr[currIdx] + " prev Ele ---> "+ arr[prevIdx]);
+
+        if( prevIdx == -1 || arr[prevIdx] < arr[currIdx])
+        {
+            accept = 1 + helper(currIdx+1,currIdx,n,arr,dp);
+        }
+
+        return dp[currIdx][prevIdx+1] = Math.max(skip,accept);
+    }
     public int lengthOfLIS(int[] nums) {
-
-        // define TreeSet for sorting & extracting unique Elements from given array
-        TreeSet<Integer> uniqueSet = new TreeSet<>();
-
-        // add given array elements into set
-        for(int i=0;i<nums.length;i++)
-        {
-            uniqueSet.add(nums[i]);
-        }
-
-        // create new array which contains sorted & unique elements
-        int[] nums1 = new int[uniqueSet.size()];
-        int i=0; // iterator
-
-        for(Integer st: uniqueSet)
-        {
-            nums1[i] = st;
-            i++;
-        }
-
-        // define new DP array
-        int dp[][] = new int[nums1.length+1][nums.length+1];
-
-        int n = dp.length;
-        int m = dp[0].length;
-
-        // fill dp array
-        for(i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                // if both num is same
-                if(nums1[i-1] == nums[j-1])
-                {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                }
-                else{ // num is not same
-
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-
-        return dp[n-1][m-1];
         
+
+        int n = nums.length;
+        int idx = 0;
+
+        int prev = -1;
+
+        int[][]dp = new int[n][n+1];
+
+        for(int[]arr : dp)
+        {
+            Arrays.fill(arr,Integer.MIN_VALUE);
+        }
+
+        return helper(0,-1,n,nums,dp);
     }
 }
