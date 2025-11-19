5689fc285939441303114f7a0b868cc1aaa250b2
Nisarg284 <128417238+Nisarg284@users.noreply.github.com>
2025-11-20 01:28:34 +0530

    Time: 12 ms (30.28%) | Memory: 55.7 MB (5.14%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,47 +1,23 @@
 class Solution {
-    public static String alphaSorting(String str) {
-        ArrayList<Character> al = new ArrayList<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
 
-        for(int i=0;i<str.length();i++)
-        {
-            al.add(str.charAt(i));
-        }
-
-        Collections.sort(al);
-        StringBuffer sb = new StringBuffer();
-
-        for(int i=0;i<al.size();i++)
-        {
-            sb.append(al.get(i));
-        }
-        return sb.toString();
-    }
-
-    public static List<List<String>> groupAnagrams(String[] strs) {
-
-        // Define hashMap of String:List
-        HashMap<String, List<String>> hm = new HashMap<>();
+        Map<String,List<String>> hm = Arrays.stream(strs)
+                                .collect(Collectors.groupingBy(
+                                    word -> {
+                                        char[] arr = word.toCharArray();
+                                        Arrays.sort(arr);
+                                        return new String(arr);
+                                    }
+                                ));
 
         List<List<String>> ans = new ArrayList<>();
 
-        for (String s : strs) {
-            String sorted = alphaSorting(s);
+        hm.values().stream()
+        .forEach(curr -> ans.add(curr));
 
-            if(!hm.containsKey(sorted))
-            {
-                List<String> l = new ArrayList<>();
-                l.add(s);
-                hm.put(sorted, l);
-            }else{
-                hm.get(sorted).add(s);
-            }
-        }
-
-        // now iterate in HashMap & store every List in List array
-        for(String s : hm.keySet())
-        {
-            ans.add(hm.get(s));
-        }
         return ans;
+        
     }
+
+
 }
