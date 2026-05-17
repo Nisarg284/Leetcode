306bab3ae0b2996f21b8982638db8778d0ef8aec
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-05-18 00:52:32 +0530

    Time: 36 ms (11.70%) | Memory: 47.1 MB (19.84%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,52 +1,45 @@
 class Solution {
 
-    public static int helper(int idx,int target,int[]coins,int[][]dp)
-    {
-        if(target == 0)
-        {
+
+    public static int helper(int idx,int amount,int[]coins,int[][]dp){
+
+         if(amount == 0){
             return 0;
         }
 
-        if(idx < 0)
-        {
+
+        if(idx < 0 || amount < 0){
             return Integer.MAX_VALUE;
         }
 
-        if(dp[idx][target] != -1)
-        {
-            return dp[idx][target];
+        if(dp[idx][amount] != -1){
+            return dp[idx][amount];
         }
 
         int take = Integer.MAX_VALUE;
-        int skip = helper(idx-1,target,coins,dp);
-        // int takeAndStay = Integer.MAX_VALUE;
+        int skip = helper(idx - 1,amount,coins,dp);
 
-        if(coins[idx] <= target)
-        {
-            int stay = helper(idx,target - coins[idx],coins,dp);
-
-            if(stay != Integer.MAX_VALUE)
-            {
+        if(amount >= coins[idx]){
+            int stay = helper(idx,amount - coins[idx],coins,dp);
+            if(stay != Integer.MAX_VALUE){
                 take = 1 + stay;
             }
-            // takeAndStay = 1 + helper(idx,target - coins[idx],coins);
         }
 
-        int findMin = Math.min(take,skip);
-        return dp[idx][target] = findMin;
+        return dp[idx][amount] = Math.min(take,skip);
     }
     public int coinChange(int[] coins, int amount) {
 
         int n = coins.length;
+        int idx = n-1;
 
         int[][]dp = new int[n][amount+1];
 
-        for(int[]arr : dp)
-        {
+        for(int[]arr : dp){
             Arrays.fill(arr,-1);
         }
 
-        int ans = helper(n-1,amount,coins,dp);
+        int ans = helper(idx,amount,coins,dp);
 
         return ans == Integer.MAX_VALUE ? -1 : ans;
         
