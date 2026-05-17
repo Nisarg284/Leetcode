b6dc0fc90b455cbec0f3ec0a620e60051a0c0ab6
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-05-18 01:49:51 +0530

    Time: 16 ms (95.79%) | Memory: 47.7 MB (31.12%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,29 +1,38 @@
 class Solution {
 
-    public static int helper(int start,int end,int[][]dp,String str)
-    {
-        if(start >= end)
-        {
+
+    public static int helper(int start,int end,String str,int[][]dp){
+        if(start >= end){
             return 0;
         }
 
-        if(dp[start][end] != 0)
-        {
+        if(dp[start][end] != 0){
             return dp[start][end];
         }
-        if(str.charAt(start) == str.charAt(end))
-        {
-            return helper(start+1,end-1,dp,str);
+
+        int match = Integer.MAX_VALUE;
+        int misMatch = Integer.MAX_VALUE;
+
+        if(str.charAt(start) == str.charAt(end)){
+            match = helper(start + 1,end - 1,str,dp);
+        }else{
+            misMatch = 1 + Math.min(helper(start+1,end,str,dp),helper(start,end-1,str,dp));
         }
 
-        int skipFirst = helper(start+1,end,dp,str);
-        int skipLast = helper(start,end-1,dp,str);
-        return dp[start][end] = 1 + Math.min( skipFirst, skipLast);
+        return dp[start][end] = Math.min(match,misMatch);
     }
     public int minInsertions(String s) {
+
         int n = s.length();
+        int start = 0;
+        int end = n-1;
+
         int[][] dp = new int[n+1][n+1];
-        return helper(0,n-1,dp,s);
+
+
+        return helper(start,end,s,dp);
+
+
         
     }
 }
