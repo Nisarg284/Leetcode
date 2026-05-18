faa8a9af6f53c0f0c1bc2ab59795770aa1ccf52e
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-05-18 21:19:42 +0530

    Time: 562 ms (27.86%) | Memory: 43 MB (84.52%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,83 +1,26 @@
 class Solution {
 
-    public static int helper(int idx,int target,int arr[],int[][]dp)
-    {
-        if(idx == 0)
-        {
-            if(arr[idx] == 0 && target == 0)
-            {
-                return 2;
-            }
-            if( target == 0 || target == arr[idx])
-            {
-                return 1;
-            }
+
+    public static int helper(int idx,int target,int[]arr){
+        if(idx < 0 && target == 0){
+            return 1;
+        }
+
+        if(idx < 0 ){
             return 0;
         }
 
-        // if(idx < 0)
-        // {
-        //     if(target == 0)
-        //     {
-        //         return 1;
-        //     }
+        int add = helper(idx - 1,target + arr[idx],arr);
+        int minus = helper(idx - 1,target - arr[idx],arr);
 
-        //     return 0;
-        // }
 
-        if(dp[idx][target] != -1)
-        {
-            return dp[idx][target];
-        }
-
-        // int plus = helper(idx-1,target - arr[idx],arr,dp,offSet);
-        // int minus = helper(idx-1,target + arr[idx],arr,dp,offSet);
-
-        int take = 0;
-        int skip = helper(idx-1,target,arr,dp);
-
-        if(arr[idx] <= target)
-        {
-            take = helper(idx-1,target - arr[idx],arr,dp);
-        }
-
-        return dp[idx][target] = take + skip;
-
-        // return dp[idx][target + offSet] =  plus + minus;
+        return add + minus;
     }
     public int findTargetSumWays(int[] nums, int target) {
 
         int n = nums.length;
 
-        int sum = 0;
-
-        for(int i = 0;i<n;i++)
-        {
-            sum+=nums[i];
-        }
-
-        if(sum - target < 0)
-        {
-            return 0;
-        }
-
-        if((sum - target) % 2 == 1)
-        {
-            return 0;
-        }
-
-        sum = (sum - target)/2;
-
-        int dp[][] = new int[n][sum+1];
-
-        for(int arr[] : dp)
-        {
-            Arrays.fill(arr,-1);
-        }
-
-        int ans = helper(n-1,sum,nums,dp);
-
-        return ans;
+        return helper(n-1,target,nums);
         
     }
 }
