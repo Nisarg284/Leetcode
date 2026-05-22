6bf2a426db38c6baef4ad136d7f2cf4030df81ca
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-05-22 15:55:50 +0530

    Time: 526 ms (5.03%) | Memory: 321.7 MB (5.01%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,60 +1,45 @@
 class Solution {
 
-    public static boolean dfsHelper(HashMap<Integer,ArrayList<Integer>> graph,int src,boolean[]vis,int destination)
-    {
-        vis[src] = true;
 
-        if(src == destination)
-        {
+    public static boolean helper(int src,int destination,boolean[] vis,HashMap<Integer,ArrayList<Integer>>graph){
+
+        vis[src] = true;
+        if(src == destination){
             return true;
         }
 
         ArrayList<Integer> destNodes = graph.get(src);
-        for(int dest : destNodes)
-        {
-            if(!vis[dest])
-            {
-                boolean flag = dfsHelper(graph,dest,vis,destination);
-                if(flag)
-                {
+        for(int dest : destNodes){
+
+            if(!vis[dest]){
+                boolean flag = helper(dest,destination,vis,graph);
+
+                if(flag){
                     return true;
                 }
             }
         }
 
         return false;
+
     }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
 
         HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
 
-        for(int[] i : edges)
-        {
-            int src = i[0];
-            int dest = i[1];
+        for(int[]arr : edges){
+            int src = arr[0];
+            int dest = arr[1];
 
-            graph.computeIfAbsent(src,k->new ArrayList<>()).add(dest);
-            graph.computeIfAbsent(dest,k->new ArrayList<>()).add(src);
+            graph.computeIfAbsent(src,k -> new ArrayList<>()).add(dest);
+            graph.computeIfAbsent(dest,k -> new ArrayList<>()).add(src);
         }
+
+        System.out.println(graph);
 
         boolean[] vis = new boolean[n];
 
-        // for(int i = 0;i<n;i++)
-        // {
-        //     if(!vis[i])
-        //     {
-               boolean flag = dfsHelper(graph,source,vis,destination);
-
-               if(flag)
-               {
-                return true;
-               }
-        //     }
-        // }
-
-        return false;
-
-
+        return helper(source,destination,vis,graph);
         
     }
 }
