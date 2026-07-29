7b526910b3a637c7ee70966942a498785be75430
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-07-29 21:10:10 +0530

    Time: 2 ms (93.95%) | Memory: 72 MB (52.82%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,52 +1,29 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-        int zeroIdx = -1;
-        int zeroCount = 0;
-        int prefixProd = 1;
-        int withoutZeroProd = 1;
-
         int n = nums.length;
-        for(int i = 0;i<n;i++)
-        {
-            if(nums[i] == 0)
-            {
-                if(zeroCount >= 1)
-                {
-                    return new int[n];
-                }else
-                {
-                    zeroCount++;
-                    zeroIdx = i;
-                    withoutZeroProd = prefixProd;
-                    prefixProd = 0;
-                }
-            }else{
-                withoutZeroProd *= nums[i];
-                prefixProd *= nums[i];
 
-            }
+        int pre = 1;
+
+        int[] res = new int[n];
+
+        for(int i = 0;i<n;i++){
+
+            res[i] = pre;
+            pre *= nums[i];
+             
         }
 
-        int[]ans = new int[n];
+        int suf = 1;
 
-        for(int i = 0;i<n;i++)
-        {
-            if(i == zeroIdx)
-            {
-                ans[i] = withoutZeroProd;
-            }else if(zeroCount < 1)
-            {
-                ans[i] = prefixProd / nums[i];
-            }
+        for(int i = n-1;i>=0;i--){
+
+            res[i] *= suf;
+
+            suf *= nums[i];
         }
 
-        System.out.println("zeroCount : "+zeroCount);
-        System.out.println("zeroIdx : "+zeroIdx);
-        System.out.println("withoutZeroProd : "+withoutZeroProd);
-        System.out.println("prefixProd : "+prefixProd);
-
-        return ans;
+        return res;
         
     }
 }
