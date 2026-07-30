d9816a8984e15588fba6b570fc6d3dbf277461d1
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-07-30 16:28:28 +0530

    Time: 7 ms (6.65%) | Memory: 46.5 MB (5.80%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int n = chars.length;
+        if(n == 1){
+            return 1;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        char curr = chars[0];
+        int count = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while(j < n){
+
+            if(curr == chars[j]){
+                count++;
+                j++;
+            }else{
+                sb.append(count > 1 ? curr +""+ count : curr);
+                i = j;
+                // j++;
+                count = 0;
+                curr = chars[i];
+            }
+        }
+
+        sb.append(count > 1 ? curr +""+ count : curr);
+
+        for(i = 0;i<sb.length();i++){
+
+            char ch = sb.charAt(i);
+            chars[i] = ch;
+        }
+
+
+        System.out.println("sb -------------> "+sb);
+
+        return sb.length();
+        
+    }
+}
