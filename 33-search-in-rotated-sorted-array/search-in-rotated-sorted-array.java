306cfc2dc7634560e55f5e9a78c48894d26534a2
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-07-31 14:54:35 +0530

    Time: 11 ms (0.97%) | Memory: 44.4 MB (11.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,39 +1,36 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int search(int[] arr, int target) {
 
-        int n = nums.length;
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
 
-        int start = 0;
-        int end = n-1;
+        while(i <= j){
 
-        while(start <= end)
-        {
-            int mid = start + (end - start)/2;
-
-
-            if(nums[mid] == target)
-            {
+            int mid = i + (j-i) / 2;
+            if(arr[mid] == target){
                 return mid;
             }
 
-            // left is sorted
-            if(nums[start] <= nums[mid])
-            {
-                if(target >= nums[start] && target < nums[mid])
-                {
-                    end = mid-1;
-                }else{
-                    start = mid+1;
-                }
-            }
-            // right is sorted
-            else{
+            System.out.println(" mid --------->"+mid+" , value : "+arr[mid]);
 
-                if(target > nums[mid] && target <= nums[end])
-                {
-                    start = mid+1;
+            if(arr[mid] >= arr[i] ){
+                System.out.print("first: ");
+                System.out.println(arr[mid] +" > "+ arr[i]);
+
+                if(target >= arr[i] && target < arr[mid]){
+                    j = mid-1;
                 }else{
-                    end = mid-1;
+                    i = mid+1;
+                }
+            }else{
+                System.out.print("second: ");
+                System.out.println(arr[mid]+ " < " +arr[i]);
+
+                if(target <= arr[j] && target > arr[mid]){
+                    i = mid+1;
+                }else{
+                    j = mid-1;
                 }
             }
         }
