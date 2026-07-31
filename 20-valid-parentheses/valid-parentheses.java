24a557a215115126bee381106ad8b812178813d5
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-07-31 15:48:10 +0530

    Time: 3 ms (86.07%) | Memory: 43.3 MB (58.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,37 +1,41 @@
 class Solution {
     public boolean isValid(String s) {
 
-        if (s.length() % 2 != 0) {
-            return false;
-        }
+        Map<Character,Character> hm = new HashMap<>();
 
         Stack<Character> st = new Stack<>();
+        int n = s.length();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        hm.put(')','(');
+        hm.put('}','{');
+        hm.put(']','[');
 
-            if (ch == '(' || ch == '[' || ch == '{') {
-                st.push(ch);
-            } else {
 
-                if (st.isEmpty()) {
+        for(int i = 0;i<n;i++){
+
+            char curr = s.charAt(i);
+            if(curr == '(' || curr == '[' || curr == '{'){
+                st.add(curr);
+            }
+            else{
+
+                if(st.isEmpty()){
                     return false;
                 }
-                char top = st.peek();
 
-                if ((top == '(' && ch == ')') || (top == '{' && ch == '}') || (top == '[' && ch == ']')) {
-                    st.pop();
-                } else {
+                if(hm.get(curr) != st.peek()){
                     return false;
+                }else{
+                    st.pop();
                 }
             }
         }
 
-        if (!st.isEmpty()) {
-            return false;
-        }
 
-        return true;
+        return st.isEmpty();
 
+
+        
+        
     }
 }
