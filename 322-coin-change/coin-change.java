c714bfcc40aafc3a217fe19e209fc58debb07d93
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-07-31 20:16:22 +0530

    Time: 38 ms (9.12%) | Memory: 47.1 MB (20.03%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,28 +1,27 @@
 class Solution {
 
+    public static int helper(int idx,int[]coins,int amount,int[][]dp){
 
-    public static int helper(int idx,int amount,int[]coins,int[][]dp){
-
-         if(amount == 0){
+        if(amount == 0){
             return 0;
         }
-
-
-        if(idx < 0 || amount < 0){
+        
+        if( idx < 0 || amount < 0){
             return Integer.MAX_VALUE;
         }
 
         if(dp[idx][amount] != -1){
             return dp[idx][amount];
+
         }
 
         int take = Integer.MAX_VALUE;
-        int skip = helper(idx - 1,amount,coins,dp);
+        int skip = helper(idx-1,coins,amount,dp);
 
-        if(amount >= coins[idx]){
-            int stay = helper(idx,amount - coins[idx],coins,dp);
+        if(coins[idx] <= amount){
+            int stay = helper(idx,coins,amount - coins[idx],dp);
             if(stay != Integer.MAX_VALUE){
-                take = 1 + stay;
+                take = 1 + stay; 
             }
         }
 
@@ -33,13 +32,13 @@ class Solution {
         int n = coins.length;
         int idx = n-1;
 
-        int[][]dp = new int[n][amount+1];
+        int[][] dp = new int[n][amount+1];
 
-        for(int[]arr : dp){
-            Arrays.fill(arr,-1);
+        for(int[] i : dp){
+            Arrays.fill(i,-1);
         }
 
-        int ans = helper(idx,amount,coins,dp);
+        int ans = helper(idx,coins,amount,dp);
 
         return ans == Integer.MAX_VALUE ? -1 : ans;
         
