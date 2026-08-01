bf40d2c5abfd7145fa56e729e6f4dbd6db196ba9
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-08-01 15:46:17 +0530

    Time: 15 ms (9.38%) | Memory: 42.4 MB (16.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,32 @@
 class Solution {
+
+    public static int helper(int prev1,int prev2,int n){
+
+        if(n == 0 || n == 1){
+            return n;
+        } 
+
+        if(prev1 != -1 && prev2 != -1){
+            return prev1 + prev2;
+        }
+
+        prev1 = helper(prev1,prev2,n-1);
+        prev2 = helper(prev1,prev2,n-2);
+
+        return prev1 + prev2;
+    }
     public int fib(int n) {
 
         if(n == 0 || n == 1){
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+        int prev1 = -1;
+        int prev2 = -1;
+
+        return helper(prev1,prev2,n);
+
+
         
     }
 }
