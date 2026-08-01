5da18ec5edef8d4e452b0a8a6ceaad09b19187d5
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-08-01 16:01:55 +0530

    Time: 2 ms (70.30%) | Memory: 45 MB (12.73%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,46 +1,33 @@
 class Solution {
 
 
-    public static void helper(int opening,int closing,List<String>ans,int n,String str)
-    {
-        if(opening == 0 && closing == 0)
-        {
-            ans.add(str);
+    public static void helper(int opening,int closing,String curr,List<String>ans){
+
+        if(opening == 0 && closing == 0){
+
+            ans.add(curr);
             return;
         }
 
-        if(opening > 0)
-        {
-            helper(opening-1,closing,ans,n,str+'(');
-            // return;
+        if(opening > 0){
+            helper(opening-1,closing,curr + '(',ans);
         }
 
-        if(closing > opening)
-        {
-            helper(opening,closing-1,ans,n,str+')');
-            // return;
+        if(closing > opening){
+            helper(opening,closing - 1,curr + ')',ans);
         }
-
-        // helper(opening-1,closing,ans,n,str+'(');
-        // helper(opening,closing-1,ans,n,str+')');
-
     }
+
     public List<String> generateParenthesis(int n) {
 
-        if(n == 0)
-        {
-            return new ArrayList<>();
-        }
-
-        int opening = n;
-        int closing = n;
+        // StringBuilder sb = new StirngBuilder();
 
         List<String> ans = new ArrayList<>();
 
-        helper(opening,closing,ans,n,"");
+
+        helper(n,n,"",ans);
 
         return ans;
-
         
     }
 }
