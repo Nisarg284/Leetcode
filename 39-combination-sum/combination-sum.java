df5d35010b434e05eba0d726fd3b74a74482e91d
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-08-02 13:05:46 +0530

    Time: 37 ms (8.07%) | Memory: 47.4 MB (5.12%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,35 +1,38 @@
 class Solution {
 
-    public void helper(int idx,int[]arr,int target,List<Integer>subAns,List<List<Integer>> ans)
-    {
-        if(target < 0)
-        {
-            return;
-        }
 
-        if(target == 0)
-        {
+    public static void helper(int idx,List<Integer> subAns,List<List<Integer>> ans,int[] candidates, int target){
+
+        if(target == 0){
             ans.add(new ArrayList<>(subAns));
             return;
         }
 
-        for(int i = idx;i<arr.length;i++)
-        {
-            subAns.add(arr[i]);
-            helper(i,arr,target-arr[i],subAns,ans);
-            subAns.removeLast();
+        if(idx >= candidates.length || target < 0){
+            return;
         }
+
+        for(int i = idx;i<candidates.length;i++){
+
+            if(target >= candidates[i]){
+                subAns.add(candidates[i]);
+                System.out.println(subAns);
+                helper(i,subAns,ans,candidates,target-candidates[i]);
+                subAns.removeLast();
+            }
+        }
+
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        int currIdx = 0;
+
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer>subAns = new ArrayList<>();
+        List<Integer> subAns = new ArrayList<>();
 
-
-
-        helper(currIdx,candidates,target,subAns,ans);
-
+        int idx = 0;
+        
+        helper(idx,subAns,ans,candidates,target);
 
         return ans;
         
