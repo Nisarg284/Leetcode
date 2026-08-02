de09a488e888fd33942b991c58af9467a6273166
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-08-02 13:53:33 +0530

    Time: 3 ms (87.53%) | Memory: 52.3 MB (60.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,74 +1,49 @@
 class Solution {
 
 
-    public void helper(Queue<int[]>q,char[][]grid,boolean[][]vis,int n,int m)
-    {
-        while(!q.isEmpty())
-        {
-            int size = q.size();
+    public static void helper(int i,int j,boolean[][]vis,char[][]grid,int n,int m){
 
-            for(int i = 0;i<size;i++)
-            {
-                int[] curr = q.remove();
-
-                int currRow = curr[0];
-                int currCol = curr[1];
-
-                if(currRow - 1 >= 0 && grid[currRow-1][currCol] == '1' && !vis[currRow-1][currCol])
-                {
-                    vis[currRow-1][currCol] = true;
-                    q.add(new int[]{currRow-1,currCol});
-                }
-
-
-                if(currCol+1 < m && grid[currRow][currCol+1] == '1' && !vis[currRow][currCol+1])
-                {
-                    vis[currRow][currCol+1] = true;
-                    q.add(new int[]{currRow,currCol+1});
-                }
-
-                if(currRow + 1 < n && grid[currRow+1][currCol] == '1' && !vis[currRow+1][currCol])
-                {
-                    vis[currRow+1][currCol] = true;
-                    q.add(new int[]{currRow+1,currCol});
-                }
-
-
-                if(currCol -1 >= 0 && grid[currRow][currCol-1] == '1' && !vis[currRow][currCol-1])
-                {
-                    vis[currRow][currCol-1] = true;
-                    q.add(new int[]{currRow,currCol-1});
-                }
-            }
+        if(i < 0 || j >= m || i >= n || j < 0 || grid[i][j] == '0' || vis[i][j] == true){
+            return;
         }
+
+        vis[i][j] = true;
+
+        // up
+        helper(i-1,j,vis,grid,n,m);
+
+        // right
+        helper(i,j+1,vis,grid,n,m);
+
+        // down
+        helper(i+1,j,vis,grid,n,m);
+
+        // left
+        helper(i,j-1,vis,grid,n,m);
     }
 
     public int numIslands(char[][] grid) {
 
-
         int n = grid.length;
         int m = grid[0].length;
+      
+        int count = 0;
 
         boolean[][] vis = new boolean[n][m];
 
-        Queue<int[]> q = new LinkedList<>();
-        int island = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
 
-        for(int i = 0;i<n;i++)
-        {
-            for(int j = 0;j<m;j++)
-            {
-                if(grid[i][j] == '1' && !vis[i][j])
-                {
-                    q.add(new int[]{i,j});
-                    island++;
-                    
-                    helper(q,grid,vis,n,m);
+                if(grid[i][j] == '1' && vis[i][j] == false){
+                    helper(i,j,vis,grid,n,m);
+                    count++;
                 }
             }
         }
 
-        return island;
+        return count;
+
+
         
     }
 }
