ff106e624a47ab92dbbef4888facf55d3aa422ef
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-08-31 21:52:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.85%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,33 +1,28 @@
 class Solution {
 
-    public static int helper(int house,int n,int[] arr,int[]dp){
-
-        if(house < 0){
+    public static int helper(int idx,int[]arr,int[]dp){
+        if(idx >= arr.length){
             return 0;
         }
 
-        if(dp[house] != -1){
-            return dp[house];
+        if(dp[idx] != Integer.MIN_VALUE){
+            return dp[idx];
         }
 
-        int stealAmount = 0;
+        int steal = arr[idx] + helper(idx + 2,arr,dp);
+        int skip = helper(idx + 1,arr,dp);
 
-        int steal = arr[house] + helper(house - 2,n,arr,dp); 
-        int skip =  helper(house - 1,n,arr,dp); 
-
-        stealAmount = Math.max(steal,skip);
-        return dp[house] = stealAmount;
+        return dp[idx] = Math.max(steal,skip);
     }
     public int rob(int[] nums) {
 
-
         int n = nums.length;
-        int house = n-1;
-        int[]dp = new int[n];
 
-        Arrays.fill(dp,-1);
+        int[]dp = new int[n+1];
 
-        return helper(house,n,nums,dp);
+        Arrays.fill(dp,Integer.MIN_VALUE);
+
+        return helper(0,nums,dp);
         
     }
 }
