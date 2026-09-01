ba13afd04540daac6ab7dc28eb66b8bb423f886e
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-01 18:18:46 +0530

    Time: 301 ms (7.19%) | Memory: 47.8 MB (81.17%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,21 +1,33 @@
 class Solution {
-    public boolean canJump(int[] nums) {
 
-
-        int maxPoint = 0;
-        int n = nums.length;
-
-        for(int i = 0;i<n;i++)
-        {
-            if(i > maxPoint)
-            {
-                return false;
-            }
-
-            maxPoint = Math.max(maxPoint, i + nums[i]);
+    public static boolean helper(int idx,int[]arr,int n,Boolean[]dp){
+        if(idx >= n-1){
+            return true;
         }
 
-        return true;
+        if(dp[idx] != null){
+            return dp[idx];
+        }
+
+        boolean flag = false;
+
+        for(int i = idx+1;i<n && i <= idx + arr[idx];i++){
+            if(helper(i,arr,n,dp)){
+                return dp[idx] = true;
+            }
+        }
+
+        return dp[idx] = flag;
+    }
+    public boolean canJump(int[] nums) {
+
+        int idx = 0;
+        int cap = 0;
+
+        Boolean[]dp = new Boolean[nums.length];
+
+        return helper(idx,nums,nums.length,dp);
+
         
     }
 }
