1e66e25ffc19eb49ed125cc110e6530524e6bd05
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-01 15:28:33 +0530

    Time: 35 ms (12.95%) | Memory: 47.2 MB (21.21%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,40 +2,48 @@ class Solution {
 
     public static int helper(int idx,int[]coins,int amount,int[][]dp){
 
+        if(idx >= coins.length || amount < 0){
+            return Integer.MAX_VALUE;
+        }
+
+        // if(idx >= coins.length){
+        //     return Integer.MAX_VALUE;
+        // }
+
+        if(dp[idx][amount] != -1){
+            return dp[idx][amount];
+        }
+
         if(amount == 0){
             return 0;
         }
         
-        if( idx < 0 || amount < 0){
-            return Integer.MAX_VALUE;
-        }
 
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
+        
 
-        }
-
+        // int need = Integer.MAX_VALUE;
+        int skip = helper(idx+1,coins,amount,dp);
         int take = Integer.MAX_VALUE;
-        int skip = helper(idx-1,coins,amount,dp);
-
         if(coins[idx] <= amount){
-            int stay = helper(idx,coins,amount - coins[idx],dp);
-            if(stay != Integer.MAX_VALUE){
-                take = 1 + stay; 
+
+            int currTake = helper(idx,coins,amount - coins[idx],dp);
+            if(currTake != Integer.MAX_VALUE){
+                take = 1 + currTake;
             }
+
         }
 
-        return dp[idx][amount] = Math.min(take,skip);
+        return dp[idx][amount] = Math.min(skip,take);
+        
     }
     public int coinChange(int[] coins, int amount) {
 
+        int idx = 0;
         int n = coins.length;
-        int idx = n-1;
+        int[][]dp = new int[n+1][amount+1];
 
-        int[][] dp = new int[n][amount+1];
-
-        for(int[] i : dp){
-            Arrays.fill(i,-1);
+        for(int[]arr : dp){
+            Arrays.fill(arr,-1);
         }
 
         int ans = helper(idx,coins,amount,dp);
