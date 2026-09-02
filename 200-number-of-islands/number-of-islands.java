981ff0fda9abb396a62bc194e84c657085e75b6d
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-02 13:26:15 +0530

    Time: 4 ms (46.77%) | Memory: 52.5 MB (32.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,48 +1,38 @@
 class Solution {
 
-
-    public static void helper(int i,int j,boolean[][]vis,char[][]grid,int n,int m){
-
-        if(i < 0 || j >= m || i >= n || j < 0 || grid[i][j] == '0' || vis[i][j] == true){
+    public static void helper(int i,int j,char[][]grid,boolean[][]vis,int n,int m){
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0' || vis[i][j]){
             return;
         }
-
         vis[i][j] = true;
 
-        // up
-        helper(i-1,j,vis,grid,n,m);
-
+        // top
+        helper(i-1,j,grid,vis,n,m);
         // right
-        helper(i,j+1,vis,grid,n,m);
-
+        helper(i,j+1,grid,vis,n,m);
         // down
-        helper(i+1,j,vis,grid,n,m);
-
+        helper(i+1,j,grid,vis,n,m);
         // left
-        helper(i,j-1,vis,grid,n,m);
+        helper(i,j-1,grid,vis,n,m);
     }
-
     public int numIslands(char[][] grid) {
 
         int n = grid.length;
         int m = grid[0].length;
-      
-        int count = 0;
-
         boolean[][] vis = new boolean[n][m];
+
+        int count = 0;
 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-
-                if(grid[i][j] == '1' && vis[i][j] == false){
-                    helper(i,j,vis,grid,n,m);
+                if(grid[i][j] == '1' && !vis[i][j]){
                     count++;
+                    helper(i,j,grid,vis,n,m);
                 }
             }
         }
 
         return count;
-
 
         
     }
