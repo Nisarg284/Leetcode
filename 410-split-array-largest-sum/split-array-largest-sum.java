8d9ea8a03215a37ad5b49afd30da710f62d16bd2
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-02 20:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (7.24%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,38 +1,44 @@
 class Solution {
 
-    public static boolean isValid(int[]arr,int k,int mid,int n)
-    {
-        int parts = 1;
+    public static boolean valid(int cap,int[]arr,int k){
+
         int sum = 0;
-        for(int i = 0;i<n;i++)
-        {
-            if(sum + arr[i] <= mid)
-            {
-                sum += arr[i];
+
+        for(int ele : arr){
+
+            if(k <= 0){
+                return false;
+            }
+
+            if(sum + ele <= cap){
+                sum += ele;
             }else{
-                parts++;
-                sum = arr[i];
+                k--;
+                sum = 0;
+                sum = ele;
             }
         }
 
-        return parts<=k;
+        return k > 0;
     }
     public int splitArray(int[] nums, int k) {
 
-        int n = nums.length;
+        int i = Integer.MIN_VALUE;
+        int j = 0;
 
-        int i = Arrays.stream(nums).max().getAsInt();
-        int j = Arrays.stream(nums).sum(); // using stream API 
+        for(int num : nums){
+            i = Math.max(i,num);
+            j += num;
+        }
 
-        while(i <= j)
-        {
-            int mid = (i + j) / 2;
+        while(i <= j){
 
-            if(isValid(nums,k,mid,n))
-            {
-                j = mid-1;
+            int mid = (i+j) / 2;
+
+            if(valid(mid,nums,k)){
+                j = mid - 1;
             }else{
-                i = mid+1;
+                i = mid + 1;
             }
         }
 
