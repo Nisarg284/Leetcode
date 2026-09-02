1544e7e35818ca39a9cd94c458bcadbde825a2ca
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-02 15:51:25 +0530

    Time: 13 ms (38.08%) | Memory: 49.2 MB (45.28%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,76 +1,69 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
 
-        HashMap<Integer,List<int[]>> graph = new HashMap<>();
+        Map<Integer,List<int[]>> graph = new HashMap<>();
 
-        for(int[] time : times)
-        {
-            int src = time[0];
-            int dest = time[1];
-            int cost = time[2];
+        for(int[] node : times){
 
-            graph.computeIfAbsent(src, key -> new ArrayList<>()).add(new int[]{dest,cost});
+            int src = node[0];
+            int dest = node[1];
+            int time = node[2];
+
+            graph.computeIfAbsent(src,j -> new ArrayList<>()).add(new int[]{dest,time});
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1] - b[1]);
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
 
-        int[] distance = new int[n+1];
+            int[] distance = new int[n+1];
 
-        Arrays.fill(distance,Integer.MAX_VALUE);
-        distance[k] = 0;
+            Arrays.fill(distance,Integer.MAX_VALUE);
 
-        boolean[]vis = new boolean[n+1];
+            boolean[] vis = new boolean[n+1];
 
-        pq.add(new int[]{k,0});
+            distance[k] = 0;
+            pq.add(new int[]{k,0});
 
-        while(!pq.isEmpty())
-        {
-            int[]srcInfo = pq.remove();
-            int src = srcInfo[0];
-            int currCost = srcInfo[1];
+            while(!pq.isEmpty()){
 
-            if(vis[src])
-            {
-                continue;
-            }
-            vis[src] = true;
+                int[]srcInfo = pq.remove();
+                int src = srcInfo[0];
+                int currCost = srcInfo[1];
 
-            
+                if(vis[src]){
+                    continue;
+                }
 
-            // Extract it's connected nodes( neighbours)
-            List<int[]> destNodes = graph.get(src);
-            if(destNodes == null)
-            {
-                continue;
-            }
+                vis[src] = true;
 
-            for(int[] destInfo : destNodes)
-            {
-                int dest = destInfo[0];
-                int pathCost = destInfo[1];
+                List<int[]> distNodes = graph.get(src);
+                if(distNodes == null){
+                    continue;
+                }
 
-                int newCost = currCost + pathCost;
+                for(int[]destInfo : distNodes){
 
-                if(newCost < distance[dest])
-                {
-                    distance[dest] = newCost;
-                    pq.add(new int[]{dest,newCost});
+                    int dest = destInfo[0];
+                    int pathCost = destInfo[1];
+
+                    int newCost = currCost + pathCost;
+
+                    if(newCost < distance[dest]){
+                        distance[dest] = newCost;
+                        pq.add(new int[]{dest,newCost});
+                    }
                 }
             }
-        }
 
-        int minTime = Integer.MIN_VALUE;
-
-        for(int i = 1;i<=n;i++)
-        {
-            if(distance[i] == Integer.MAX_VALUE)
-            {
-                return -1;
+            
+            int minCost = Integer.MIN_VALUE;
+            for(int i = 1;i<=n;i++){
+                if(distance[i] == Integer.MAX_VALUE){
+                    return -1;
+                }
+                minCost = Math.max(minCost,distance[i]);
             }
-            minTime = Math.max(minTime,distance[i]);
-        }
 
-        return minTime;
+            return minCost;
+        }
         
     }
-}
