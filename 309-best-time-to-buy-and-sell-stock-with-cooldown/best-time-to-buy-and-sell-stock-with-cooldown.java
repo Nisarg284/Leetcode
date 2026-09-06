ddd5dba7bf5e68e7adad6e18471b5a00914f23b1
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-06 22:43:33 +0530

    Time: 210 ms (5.24%) | Memory: 237.4 MB (5.11%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,52 +1,36 @@
 class Solution {
 
+    public static int helper(int idx,int buyIdx,int n,int[]arr,Integer[][]dp){
 
-    public static int helper(int idx,int status,int n,int[]prices,int[][]dp)
-    {
-        if(idx >= n)
-        {
+        if(idx >= n){
             return 0;
         }
 
-        if(dp[idx][status] != -1)
-        {
-            return dp[idx][status];
+        if(dp[idx][buyIdx + 1] != null){
+            return dp[idx][buyIdx + 1];
         }
 
-
-        int profit = 0;
-
-        if(status == 1)
-        {
-            int buy = -prices[idx] + helper(idx+1,0,n,prices,dp);
-            int notBuy = helper(idx+1,1,n,prices,dp);
-
-
-            profit = Math.max(buy,notBuy);
+        int skip = helper(idx + 1,buyIdx,n,arr,dp);
+        int buy = Integer.MIN_VALUE;
+        int sell = Integer.MIN_VALUE;
+        if(buyIdx == -1){
+            buy = -arr[idx] + helper(idx + 1,idx,n,arr,dp);
         }else{
-            int sell = prices[idx] + helper(idx+2,1,n,prices,dp);
-            int notSell = helper(idx+1,0,n,prices,dp);
-
-            profit = Math.max(sell,notSell);
+            sell = arr[idx] + helper(idx + 2,-1,n,arr,dp);
         }
 
-        return dp[idx][status] = profit;
+        return dp[idx][buyIdx + 1] = Math.max(skip,Math.max(buy,sell));
     }
     public int maxProfit(int[] prices) {
 
-        int n = prices.length;
-
         int idx = 0;
-        int status = 1;
 
-        int[][] dp = new int[n][2];
+        int n = prices.length;
+        int buyIdx = -1;
 
-        for(int[]arr : dp)
-        {
-            Arrays.fill(arr,-1);
-        }
+        Integer[][] dp = new Integer[n][n+1];
 
-        return helper(idx,status,n,prices,dp);
+        return helper(idx,buyIdx,n,prices,dp);
         
     }
 }
