06fccf4fbd706199eed971c14d5278b98ea173b7
Nisarg Vilashbhai Solanki <128417238+Nisarg284@users.noreply.github.com>
2026-09-08 21:57:29 +0530

    Time: 27 ms (31.33%) | Memory: 46.2 MB (9.99%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,53 +1,45 @@
 class Solution {
+    public static int helper(int i,int j,int start,int end,int[]arr,Integer[][]dp){
 
-    public static int helper(int i,int j,int lower,int upper,int[]cuts,int[][]dp)
-    {
-        if(i > j)
-        {
+        if(i > j){
             return 0;
         }
 
-        if(dp[i][j] != -1)
-        {
+        if(dp[i][j] != null){
             return dp[i][j];
         }
 
-        int minCost = Integer.MAX_VALUE;
+        int mini = (int)1e9;
 
-        for(int k = i;k<=j;k++)
-        {
-            // System.out.println("upper: "+upper+", lower: "+ lower);
-           int currCost = (upper - lower) 
-           + helper(i,k-1,lower,cuts[k],cuts,dp) 
-           + helper(k+1,j,cuts[k],upper,cuts,dp);
+        for(int k = i;k<=j;k++){
+            int currCost = end - start;
 
-           minCost = Math.min(minCost,currCost);
+            int left = helper(i , k-1,start,arr[k],arr,dp);
+            int right = helper(k + 1 , j,arr[k],end,arr,dp);
+
+            int totalCost = currCost + left + right;
+            mini = Math.min(mini,totalCost);
         }
 
-        return dp[i][j] = minCost;
+        return dp[i][j] = mini;
     }
     public int minCost(int n, int[] cuts) {
 
+        int len = cuts.length;
 
         int i = 0;
-        int j = cuts.length-1;
-
-        int lower = 0;
-        int upper = n;
+        int j = len - 1;
 
         Arrays.sort(cuts);
 
-        int len = cuts.length;
+        int start = 0;
+        int end = n;
 
+        Integer[][]dp = new Integer[len][len];
 
-        int[][]dp = new int[len][len];
+        return helper(i,j,start,end,cuts,dp);
 
-        for(int[]a : dp)
-        {
-            Arrays.fill(a,-1);
-        }
-
-        return helper(i,j,lower,upper,cuts,dp);
+        
         
     }
 }
